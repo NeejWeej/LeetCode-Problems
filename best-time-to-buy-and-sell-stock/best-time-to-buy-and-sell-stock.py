6ce0601d9ce9ed014eb7bc54de4cc8de3d9f4986
NeejWeej <nijat.khanbabayev@gmail.com>
2021-08-04 05:28:47 -0400

    Time: 996 ms (72.61%), Space: 25 MB (95.66%) - LeetHub

---
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,21 @@
+class Solution:
+    # def maxProfit_recurse(self,prices):
+    #     if len(prices) == 1:
+    #         return [prices[0],prices[0],0]
+    #     prev_ans = self.maxProfit_recurse(prices[1:])
+    #     print(prev_ans)
+    #     if prev_ans[1] - prices[0] > prev_ans[-1]:
+    #         return [prices[0],prev_ans[1],prev_ans[1] - prices[0]]
+    #     return prev_ans
+    
+    def maxProfit(self, prices: List[int]) -> int:
+        best = 0
+        minval = float('inf')
+        for idx, val in enumerate(prices):
+            if val < minval:
+                minval = val
+            elif val-minval > best:
+                best = val-minval
+        return best
+                
+        
