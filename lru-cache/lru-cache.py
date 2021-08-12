d761f43398db0db3cdaf417981571d75a5af78b4
NeejWeej <nijat.khanbabayev@gmail.com>
2021-08-12 03:11:49 -0400

    Time: 724 ms (97.65%), Space: 73.4 MB (93.74%) - LeetHub

---
+++ lru-cache/lru-cache.py
@@ -0,0 +1,30 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.dict = {}
+        self.size = capacity
+
+    def get(self, key: int) -> int:
+        if key in self.dict:
+            res = self.dict.pop(key)
+            self.dict[key] = res
+            return res
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.dict:
+            res = self.dict.pop(key)
+            self.dict[key] = value
+            return
+        else:
+            if len(self.dict) >= self.size:
+                k = next(iter(self.dict))
+                del self.dict[k]
+            self.dict[key] = value
+            return
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
