85e271d8e11bb6a5c0c7b8e2036897ce392e555a
NeejWeej <nijat.khanbabayev@gmail.com>
2021-08-25 11:16:13 -0400

    Time: 916 ms (97.91%), Space: 25.1 MB (52.58%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -8,14 +8,23 @@ class Solution:
     #         return [prices[0],prev_ans[1],prev_ans[1] - prices[0]]
     #     return prev_ans
     
+        # best = 0
+        # minval = float('inf')
+        # for idx, val in enumerate(prices):
+        #     if val < minval:
+        #         minval = val
+        #     elif val-minval > best:
+        #         best = val-minval
+        # return best
+    
     def maxProfit(self, prices: List[int]) -> int:
         best = 0
-        minval = float('inf')
-        for idx, val in enumerate(prices):
-            if val < minval:
-                minval = val
-            elif val-minval > best:
-                best = val-minval
+        min_price = float('inf')
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            elif price - min_price > best:
+                best = price - min_price
         return best
                 
         
