325d3277ad6b8fbe99b97995a83ae715c7ce61e2
NeejWeej <nijat.khanbabayev@gmail.com>
2021-09-06 23:24:22 -0400

    Time: 176 ms (13.20%), Space: 15.7 MB (69.95%) - LeetHub

--- course-schedule/course-schedule.py
+++ course-schedule/course-schedule.py
@@ -5,20 +5,26 @@ class Solution:
         for p in prerequisites:
             is_prereq[p[1]].append(p[0])
             has_prereq[p[0]] += 1
+        c2 = len(has_prereq.items())
         start = []
-        for c in range(numCourses):
-            if has_prereq[c] == 0:
-                start.append(c)
-        count = 0
+        # for c in range(numCourses):
+        #     if has_prereq[c] == 0:
+        #         start.append(c)
+        for p in is_prereq.keys():
+            if has_prereq[p] == 0:
+                start.append(p)
+        # count = 0
         visited = set()
         while start:
             curr_class = start.pop()
-            count += 1
+            # count += 1
             for c in is_prereq[curr_class]:
                 if c in visited:
                     continue
                 has_prereq[c] -= 1
                 if has_prereq.get(c) == 0:
+                    c2 -= 1
                     visited.add(c)
-                    start.append(c)          
-        return True if count == numCourses else False
+                    start.append(c)  
+        # print(c2)
+        return True if c2 == 0 else False
