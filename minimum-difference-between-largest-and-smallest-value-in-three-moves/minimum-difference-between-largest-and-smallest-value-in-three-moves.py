c5f5a265c2451f5f5711fe26329fbadc9f2990b3
NeejWeej <nijat.khanbabayev@gmail.com>
2021-09-23 16:53:22 -0400

    Time: 340 ms (76.46%), Space: 24.4 MB (10.29%) - LeetHub

--- minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.py
+++ minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -1,16 +1,32 @@
 class Solution:
+    import heapq
     def minDifference(self, nums: List[int]) -> int:
         if len(nums) <= 4:
             return 0
-        biggest = []
-        neg_nums = [-num for num in nums]
-        heapq.heapify(neg_nums)
-        for i in range(4):
-            biggest.append(-heapq.heappop(neg_nums))
-        smallest = []
+        small = []
         heapq.heapify(nums)
         for i in range(4):
-            smallest.append(heapq.heappop(nums)) 
-        # print(biggest, smallest)
-        return min([x - y for x,y in zip(biggest, smallest[::-1])])
+            small.append(heapq.heappop(nums))
+        
+        nums = [-x for x in nums]
+        for val in small:
+            nums.append(-val)
+        heapq.heapify(nums)
+        big = []
+        for i in range(4):
+            big.append(-heapq.heappop(nums))
+        return min([x-y for x,y in zip(big, small[::-1])])
+        
+        
+#         biggest = []
+#         neg_nums = [-num for num in nums]
+#         heapq.heapify(neg_nums)
+#         for i in range(4):
+#             biggest.append(-heapq.heappop(neg_nums))
+#         smallest = []
+#         heapq.heapify(nums)
+#         for i in range(4):
+#             smallest.append(heapq.heappop(nums)) 
+#         # print(biggest, smallest)
+#         return min([x - y for x,y in zip(biggest, smallest[::-1])])
         
