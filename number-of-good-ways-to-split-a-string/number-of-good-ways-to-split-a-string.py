7a05859aed7a438787960e8d00d67a35522432c2
NeejWeej <nijat.khanbabayev@gmail.com>
2021-09-25 13:43:45 -0400

    Time: 148 ms (91.69%), Space: 15.6 MB (27.73%) - LeetHub

--- number-of-good-ways-to-split-a-string/number-of-good-ways-to-split-a-string.py
+++ number-of-good-ways-to-split-a-string/number-of-good-ways-to-split-a-string.py
@@ -1,26 +1,78 @@
 class Solution:
     def numSplits(self, s: str) -> int:
-        distinct_letters = set()
-        ul = [0] * len(s)
-        count = 0
-        for idx, char in enumerate(s):
-            if char not in distinct_letters:
-                distinct_letters.add(char)
-                count += 1
-            ul[idx] = count
+        left = [0] * len(s)
+        left_set = set()
+        for idx, val in enumerate(s):
+            if val not in left_set:
+                left_set.add(val)
+            left[idx] = len(left_set)
         
-        other_way = [0] * len(s)
-        distinct_letters= set()
-        count = 0
-        for idx, char in enumerate(reversed(s)):
-            if char not in distinct_letters:
-                distinct_letters.add(char)
-                count += 1
-            other_way[idx] = count
+        left_set = set()
+        
         ans = 0
-        for idx, val in enumerate(ul[:-1]):
-            if other_way[ -idx - 2] == val:
+        for idx, val in enumerate(s[::-1]):
+            actual_idx = len(s) -idx - 1
+            if left[actual_idx] == len(left_set):
                 ans += 1
+            if val not in left_set:
+                left_set.add(val)
+        
         return ans
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         distinct_letters = set()
+#         ul = [0] * len(s)
+#         count = 0
+#         for idx, char in enumerate(s):
+#             if char not in distinct_letters:
+#                 distinct_letters.add(char)
+#                 count += 1
+#             ul[idx] = count
+        
+#         other_way = [0] * len(s)
+#         distinct_letters= set()
+#         count = 0
+#         for idx, char in enumerate(reversed(s)):
+#             if char not in distinct_letters:
+#                 distinct_letters.add(char)
+#                 count += 1
+#             other_way[idx] = count
+#         ans = 0
+#         for idx, val in enumerate(ul[:-1]):
+#             if other_way[ -idx - 2] == val:
+#                 ans += 1
+#         return ans
             
         
