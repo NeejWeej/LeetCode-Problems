52385b7977338453a45494d11dfa74457c0cc497
NeejWeej <nijat.khanbabayev@gmail.com>
2021-10-04 20:10:37 -0400

    Time: 57 ms (7.52%), Space: 14.2 MB (84.33%) - LeetHub

--- decode-ways/decode-ways.py
+++ decode-ways/decode-ways.py
@@ -3,33 +3,36 @@ class Solution:
         n = len(s)
         if s[0] == '0':
             return 0
-        if len(s) == 1:
+        if n == 1:
             return 1
-        dp = [0] * n
-        dp[0] = 1
-        dp[1] = 0 if s[1] == '0' else 1
-        if int(s[:2]) <= 26:
-            dp[1] += 1
-        for idx, val in enumerate(s[2:], 2):
-            single_digit = 0 if val == '0' else dp[idx - 1]
-
-            double = int(s[idx - 1: idx + 1])
-            double_digit = 0 if (double > 26 or s[idx-1] == '0') else dp[idx - 2]
+        # dp = [0] * n
+        # dp[0] = 1
+        # dp[1] = 0 if s[1] == '0' else 1
+        # if int(s[:2]) <= 26:
+        #     dp[1] += 1
             
-            dp[idx] = double_digit + single_digit
-        return dp[-1]
-            
+        two_ago = 1
+        one_ago = 0 if s[1] == '0' else 1
         
-        # n = len(s)
-        # if (n == 1 and s[0] != '0') or n == 0:
-        #     return 1
-        # if s[0] == '0':
-        #     return 0
-        # first_two = int(s[:2])
-        # ans = 0
-        # ans += self.numDecodings(s[1:])
-        # if 10 <= first_two and first_two <= 26:
-        #     ans += self.numDecodings(s[2:])
-        # return ans
+        if int(s[:2]) <= 26:
+            one_ago += 1
+        
+        cur = one_ago
+        for idx, val in enumerate(s[2:], 2):
+            # single_digit = 0 if val == '0' else dp[idx - 1]
+            
+            single_digit = 0 if val == '0' else one_ago
+            
+            double = int(s[idx - 1: idx + 1])
+            # double_digit = 0 if (double > 26 or s[idx-1] == '0') else dp[idx - 2]
+            
+            double_digit = 0 if (double > 26 or s[idx-1] == '0') else two_ago
+            
+            # dp[idx] = double_digit + single_digit
+            cur = double_digit + single_digit
+            two_ago = one_ago
+            one_ago = cur
+        return cur
+        # return dp[-1]
     
         
