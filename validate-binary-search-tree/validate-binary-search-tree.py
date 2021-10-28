8baaaffd289a12b7af616c97421355cd23555411
NeejWeej <nijat.khanbabayev@gmail.com>
2021-10-27 22:31:07 -0400

    Time: 40 ms (93.12%), Space: 16.4 MB (54.60%) - LeetHub

--- validate-binary-search-tree/validate-binary-search-tree.py
+++ validate-binary-search-tree/validate-binary-search-tree.py
@@ -4,19 +4,24 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def isValid(self, root):
-        # returns [isBST, min, max]
-        if not root:
-            return [True, float('inf'), float('-inf')]
-        left_side = self.isValid(root.left)
-        right_side = self.isValid(root.right)
-        if left_side[0] and right_side[0]:
-            if root.val > left_side[2] and root.val < right_side[1]:
-                new_min = min(root.val, left_side[1], right_side[1])
-                new_max = max(root.val, left_side[2], right_side[2])
-                return [True, new_min, new_max]
-        return [False, float('inf'), float('-inf')]
+class Solution: 
+    import collections
+    
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        return self.isValid(root)[0]
+        if not root.left and not root.right:
+            return True
+        q = collections.deque([(root, float('-inf'), float('inf'))])
+        while q:
+            cur, low_b, up_b = q.popleft()
+            if cur.val <= low_b or up_b <= cur.val:
+                return False
+            if cur.left:
+                new_left = (cur.left, low_b, cur.val)
+                q.append(new_left)
+            if cur.right:
+                new_right = (cur.right, cur.val, up_b)
+                q.append(new_right)
+        return True
+        
+                
         
