b3d08e6ade9c24fe3da0fbbccd988ad01ac99dbc
NeejWeej <nijat.khanbabayev@gmail.com>
2021-11-28 21:31:27 -0500

    Time: 888 ms (68.51%), Space: 17.2 MB (92.47%) - LeetHub

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -1,48 +1,96 @@
 class Solution(object):
     def threeSum(self,nums):
         n = len(nums)
-        counts = {}
-        for num in nums:
-            counts[num] = counts.get(num, 0) + 1
+        nums.sort()
         
         ans = set()
         
-        for idx, val in enumerate(nums):
-            counts[val] -= 1
-            if counts[val] <= 0:
-                counts.pop(val)
-            
-            for num, count in counts.items():
-                if -val - num == num:
-                    if -val-num in counts and count > 1:
-                        new = [num, val, -val-num]
-                        new.sort()
-                        ans.add(tuple(new))                    
-                    
-                elif -val-num in counts:
-                    new = [num, val, -val-num]
-                    new.sort()
-                    ans.add(tuple(new))
+        # counts = {}
+        # for num in nums:
+        #     counts[num] = counts.get(num,0) + 1
         
+        seen = set()
+        
+        for i in range(n):
+            cur_num = nums[i]
+            if cur_num in seen:
+                continue
+            # counts[cur_num] -= 1
+            # if counts[cur_num] <= 0:
+            #     del counts[cur_num]           
+            left = i + 1
+            right = n - 1
+            while left < right:
+                new_sum =  nums[left] + nums[right] + cur_num
+                if new_sum == 0:
+                    ans.add((cur_num, nums[left], nums[right]))
+                    left += 1 
+                elif new_sum < 0:
+                    left += 1   
+                elif new_sum > 0:
+                    right -= 1
+            seen.add(cur_num)
         return list(ans)
+                
         
         
         
         
         
-        # nums.sort()
-        # res = set()
-        # for idx, val in enumerate(nums):
-        #     #since sorted, we will only run into repeats in a continguous block
-        #     if idx != 0 and val == nums[idx-1]:
-        #         continue
-        #     if idx >= n-2:
-        #         break
-        #     sums = set()
-        #     m = nums[idx+1:]
-        #     for i,v in enumerate(m):
-        #         if -val-v in sums:
-        #             res.add((-val-v,val,v))
-        #         else:
-        #             sums.add(v)
-        # return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         n = len(nums)
+#         counts = {}
+#         for num in nums:
+#             counts[num] = counts.get(num, 0) + 1
+        
+#         ans = set()
+        
+#         for idx, val in enumerate(nums):
+#             counts[val] -= 1
+#             if counts[val] <= 0:
+#                 counts.pop(val)
+            
+#             for num, count in counts.items():
+#                 if -val - num == num:
+#                     if count > 1:
+#                         new = [num, val, -val-num]
+#                         new.sort()
+#                         ans.add(tuple(new))                    
+                    
+#                 elif -val-num in counts:
+#                     new = [num, val, -val-num]
+#                     new.sort()
+#                     ans.add(tuple(new))
+        
+#         return list(ans)
+        
