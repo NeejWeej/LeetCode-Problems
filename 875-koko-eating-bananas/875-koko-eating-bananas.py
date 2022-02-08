dcaf23b5f51097ef7ac733a3bf0e162e175c3080
NeejWeej <nijat.khanbabayev@gmail.com>
2022-02-08 01:31:52 -0500

    Time: 549 ms (64.51%), Space: 15.5 MB (60.47%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.py
@@ -0,0 +1,36 @@
+class Solution:
+    
+    def can_complete(self, piles, h, k):
+        cur_hours = 0
+        for pile in piles:
+            cur_hours += (pile // k)
+            if pile % k != 0:
+                cur_hours += 1
+            if cur_hours > h:
+                return False
+        return True
+        
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        if self.can_complete(piles, h, 1):
+            return 1
+        cannot = 1
+        can = cannot
+        while True:
+            can *= 2
+            if self.can_complete(piles, h, can):
+                break
+            cannot = can
+        start = cannot
+        end = can
+        while start + 1 < end:
+            mid = start + (end - start) // 2
+            if self.can_complete(piles, h, mid):
+                end = mid
+            else:
+                start = mid
+        if self.can_complete(piles, h, start):
+            return start
+        return end
+        
+        
+        
