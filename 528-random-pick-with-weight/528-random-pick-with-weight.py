9eadc78ff404787ea8a498b34efb83ded914fb64
NeejWeej <nijat.khanbabayev@gmail.com>
2022-02-20 22:23:04 -0500

    Time: 292 ms (69.79%), Space: 18.6 MB (82.72%) - LeetHub

--- 528-random-pick-with-weight/528-random-pick-with-weight.py
+++ 528-random-pick-with-weight/528-random-pick-with-weight.py
@@ -11,7 +11,7 @@ class Solution:
         in_prefix = random.randrange(1, self.tot + 1)
         start = 0
         end = len(self.prefix_sum)
-        while start < end:
+        while start + 1 < end:
             mid = start + (end - start) // 2
             if in_prefix == self.prefix_sum[mid]:
                 return mid
@@ -23,7 +23,7 @@ class Solution:
         if start == end:
             return start
         if start + 1 == end:
-            if in_prefix == self.prefix_sum[start]:
+            if in_prefix <= self.prefix_sum[start]:
                 return start
             return end
         
