ee3b8a3c9255b2b75b47593dde98839233d5eae1
NeejWeej <nijat.khanbabayev@gmail.com>
2022-02-20 21:48:28 -0500

    Time: 48 ms (39.02%), Space: 13.8 MB (84.95%) - LeetHub

---
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        offset = 0
+        ans = 0
+        for char in s:
+            if char == '(':
+                offset += 1
+            elif char == ')':
+                offset -= 1
+            if offset == -1:
+                ans += 1
+                offset += 1
+        return ans + offset
