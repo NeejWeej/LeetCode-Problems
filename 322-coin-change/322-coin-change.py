dd6eeabd3531be6581d4626cce63bcedf7c4608b
NeejWeej <nijat.khanbabayev@gmail.com>
2022-03-02 04:59:30 -0500

    Time: 856 ms (96.46%), Space: 14.9 MB (30.57%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,5 +1,6 @@
 class Solution:
     from collections import deque
+    import random
     def coinChange(self, coins: List[int], amount: int) -> int:
         if amount == 0:
             return 0
@@ -9,6 +10,7 @@ class Solution:
         while q:
             count += 1
             new_layer = deque([])
+            random.shuffle(coins)
             for _ in range(len(q)):
                 cur = q.popleft()
                 for coin in coins:
