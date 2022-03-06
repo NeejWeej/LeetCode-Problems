c10d740007754924794ec254a0d6eb7fb8e63d50
NeejWeej <nijat.khanbabayev@gmail.com>
2022-03-06 06:08:55 -0500

    Time: 156 ms (55.84%), Space: 14.7 MB (91.32%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -4,24 +4,33 @@ class Solution:
         best = [float('inf'), '']
         if len(t) == 1 and t in s:
             return t
-        counts = Counter(t)
+        counts = {}
+        stor_counts = {}
+        for c in t:
+            counts[c] = 1 + counts.get(c,0)
+            stor_counts[c] = 1 + stor_counts.get(c,0)
+        tot_count = len(t)
         # print(counts)
         start = 0
         end = 0
         cur_counter = {}
         while end < len(s):
             cur_counter[s[end]] = 1 + cur_counter.get(s[end], 0)
-            valid = True
-            for char, num in counts.items():
-                if cur_counter.get(char, 0) < num:
-                    valid = False 
-                    break
-            if valid:
+            if counts.get(s[end], 0) > 0:
+                counts[s[end]] = counts.get(s[end]) - 1
+                tot_count -= 1
+            # counts[s[end]] = counts.get(s[end], 0) - 1
+            # valid = True
+            # for char, num in counts.items():
+            #     if cur_counter.get(char, 0) < num:
+            #         valid = False 
+            #         break
+            if tot_count == 0:
                 # print(start, end, cur_counter)
                 while start < end:
                     if end - start + 1 < best[0]:
                         best = [end - start + 1, s[start:end + 1]]
-                    if cur_counter[s[start]] <= counts.get(s[start], float('-inf')):
+                    if cur_counter[s[start]] <= stor_counts.get(s[start], float('-inf')):
                         break
                     cur_counter[s[start]] = cur_counter.get(s[start]) - 1
                     start += 1
