e1c6c601d5b48a11a1464f0332368667972bf4ff
NeejWeej <nijat.khanbabayev@gmail.com>
2022-03-12 16:41:32 -0500

    Time: 155 ms (53.67%), Space: 18.2 MB (36.48%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -7,7 +7,7 @@ import heapq as hq
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         start = [(x.val, idx, x) for idx, x in enumerate(lists) if x]
-        counter = len(lists)
+        counter = itertools.count(len(lists))
         hq.heapify(start)
         dummy = ListNode()
         cur = dummy
@@ -17,6 +17,6 @@ class Solution:
             cur = nextt
             if nextt.next:
                 nextt = nextt.next
-                hq.heappush(start, (nextt.val, counter + 1, nextt))
-                counter += 1
+                count = next(counter)
+                hq.heappush(start, (nextt.val, count, nextt))
         return dummy.next
