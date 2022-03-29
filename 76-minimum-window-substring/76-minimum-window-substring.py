928e268d8e23e4afadd0c384874dba3ca0f623ae
NeejWeej <nijat.khanbabayev@gmail.com>
2022-03-29 01:50:31 -0400

    Time: 414 ms (17.19%), Space: 14.7 MB (39.51%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,34 +1,32 @@
 class Solution:
     from collections import Counter
     def minWindow(self, s: str, t: str) -> str:
-        best = [float('inf'), '']
-        if len(t) == 1 and t in s:
-            return t
-        counts = {}
-        stor_counts = {}
-        for c in t:
-            counts[c] = 1 + counts.get(c,0)
-            stor_counts[c] = 1 + stor_counts.get(c,0)
-        tot_count = len(t)
-        # print(counts)
+        best = None
+        tCounts = Counter(t)
+        cur_chars = {}
+        cur_completed = 0
         start = 0
         end = 0
-        cur_counter = {}
-        while end < len(s):
-            cur_counter[s[end]] = 1 + cur_counter.get(s[end], 0)
-            if counts.get(s[end], 0) > 0:
-                counts[s[end]] = counts.get(s[end]) - 1
-                tot_count -= 1
-            if tot_count == 0:
-                # print(start, end, cur_counter)
-                while start < end:
-                    if end - start + 1 < best[0]:
-                        best = [end - start + 1, s[start:end + 1]]
-                    if cur_counter[s[start]] <= stor_counts.get(s[start], float('-inf')):
-                        break
-                    cur_counter[s[start]] = cur_counter.get(s[start]) - 1
-                    start += 1
-            end += 1
-        return best[1]
+        for idx, c in enumerate(s):
+            cur_val = cur_chars.get(c, 0)
+            cur_chars[c] = cur_val + 1
+            if cur_val == tCounts.get(c, 0) - 1:
+                cur_completed += 1
+            if cur_completed != len(tCounts):
+                continue
+            schar = s[start]
+            while cur_chars.get(schar) > tCounts.get(schar, 0):
+                cur_chars[schar] = cur_chars.get(schar) - 1
+                start += 1
+                schar = s[start]
+            if not best:
+                best = s[start: idx + 1]
+            elif len(best) > len(s[start: idx + 1]):
+                best = s[start: idx + 1]
+        # print(tCounts, cur_chars, s[start:])
+        return best if best else ""
+            
+                
+                
                 
             
