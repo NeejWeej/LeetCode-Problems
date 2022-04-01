5c7a8557fcf59a43bf5bb347be0a891b2277319c
NeejWeej <nijat.khanbabayev@gmail.com>
2022-03-31 21:29:38 -0400

    Time: 788 ms (96.42%), Space: 20.8 MB (6.68%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,12 +1,39 @@
+import heapq as hq
+from random import randrange
 class Solution:
-    import heapq
+    
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        new_tups = [(x*x + y*y, x, y) for x,y in points]
-        heapq.heapify(new_tups)
+        points_dist = [(x, y, x*x + y*y) for x,y in points]
+        
+        def recurse(part, check):
+            idx = randrange(0, len(part))
+            x1, y1, dist1 = part[idx][:]
+            left, right = [], []
+            for i, point in enumerate(part):
+                x, y, dist = point[:]
+                if i == idx:
+                    continue
+                elif dist <= dist1:
+                    left.append(point)
+                else:
+                    right.append(point)
+            # print(left, right, check, (x1, y1))
+            if len(left) == check - 1:
+                return (x1, y1, dist1)
+                # ans = [[x, y] for x,y,dist in left]
+                # ans.append([x1, y1])
+                # return ans
+            if len(left) < check - 1:
+                return recurse(right, check - len(left) - 1)
+            else:
+                return recurse(left, check)
+        x_key, y_key, dist_key = recurse(points_dist, k)
         ans = []
-        for _ in range(k):
-            new_closest = heapq.heappop(new_tups)
-            dist, x, y = new_closest[:]
-            ans.append([x,y])
+        for point in points_dist:
+            x,y,dist = point[:]
+            if dist <= dist_key:
+                ans.append([x, y])
         return ans
+        
+        
         
