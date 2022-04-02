20050360912f736e82d3952353fe0f3789daa58a
NeejWeej <nijat.khanbabayev@gmail.com>
2022-04-02 06:16:41 -0400

    Time: 878 ms (79.95%), Space: 24.7 MB (5.31%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -5,35 +5,48 @@ class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         points_dist = [(x, y, x*x + y*y) for x,y in points]
         
-        def recurse(part, check):
-            idx = randrange(0, len(part))
-            x1, y1, dist1 = part[idx][:]
-            left, right = [], []
-            for i, point in enumerate(part):
-                x, y, dist = point[:]
-                if i == idx:
-                    continue
-                elif dist <= dist1:
-                    left.append(point)
-                else:
-                    right.append(point)
-            # print(left, right, check, (x1, y1))
-            if len(left) == check - 1:
-                return (x1, y1, dist1)
-                # ans = [[x, y] for x,y,dist in left]
-                # ans.append([x1, y1])
-                # return ans
-            if len(left) < check - 1:
-                return recurse(right, check - len(left) - 1)
-            else:
-                return recurse(left, check)
-        x_key, y_key, dist_key = recurse(points_dist, k)
-        ans = []
-        for point in points_dist:
-            x,y,dist = point[:]
-            if dist <= dist_key:
-                ans.append([x, y])
-        return ans
+        def recurse(check, start, end):
+            idx = randrange(start, end + 1)
+            x1, y1, dist1 = points_dist[idx][:]
+            points_dist[idx], points_dist[end] = points_dist[end], points_dist[idx]
+            starting = start
+            for i in range(start, end):
+                if points_dist[i][-1] <= dist1:
+                    points_dist[i], points_dist[starting] = points_dist[starting], points_dist[i]
+                    starting += 1
+            points_dist[end], points_dist[starting] = points_dist[starting], points_dist[end]
+            if starting == check - 1:
+                return
+            elif starting < check - 1:
+                return recurse(check, starting + 1, end)
+            return recurse(check, start, starting - 1)
+        # def recurse(part, check):
+        #     idx = randrange(0, len(part))
+        #     x1, y1, dist1 = part[idx][:]
+        #     left, right = [], []
+        #     for i, point in enumerate(part):
+        #         x, y, dist = point[:]
+        #         if i == idx:
+        #             continue
+        #         elif dist <= dist1:
+        #             left.append(point)
+        #         else:
+        #             right.append(point)
+        #     if len(left) == check - 1:
+        #         return (x1, y1, dist1)
+        #     if len(left) < check - 1:
+        #         return recurse(right, check - len(left) - 1)
+        #     else:
+        #         return recurse(left, check)
+        # x_key, y_key, dist_key = recurse(points_dist, k)
+        recurse(k, 0, len(points) - 1)
+        return [points_dist[i][:-1] for i in range(k)]
+        # ans = []
+        # for point in points_dist:
+        #     x,y,dist = point[:]
+        #     if dist <= dist_key:
+        #         ans.append([x, y])
+        # return ans
         
         
         
