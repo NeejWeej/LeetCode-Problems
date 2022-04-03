52b5cec7b47c81b0e3a1a485aadf821e9403ce9b
NeejWeej <nijat.khanbabayev@gmail.com>
2022-04-02 23:09:03 -0400

    Time: 176 ms (28.41%), Space: 15.8 MB (27.51%) - LeetHub

--- 224-basic-calculator/224-basic-calculator.py
+++ 224-basic-calculator/224-basic-calculator.py
@@ -1,53 +1,43 @@
 class Solution:
-    def in_paren(self, stack, s, idx):
+    
+    def combine(self, overall, val, pos):
+        if pos:
+            overall += val
+        else:
+            overall -= val
+        return overall
+            
+    def in_paren(self, s, idx):
         #starts after paren
-        new_stack = []
         cur_num = 0
+        overall_num = 0
+        pos = True
         while idx < len(s):
             char = s[idx]
             if char.isdigit():
                 cur_num *= 10
                 cur_num += int(char)
             elif char == '(':
-                new_idx = self.in_paren(new_stack, s, idx + 1)
+                val, new_idx = self.in_paren(s, idx + 1)[:]
                 idx = new_idx
-                cur_num = 0
+                overall_num = self.combine(overall_num, val, pos)
             elif char == '+':
-                if cur_num != 0:
-                    new_stack.append(cur_num)
-                new_stack.append('+')
+                overall_num = self.combine(overall_num, cur_num, pos)
+                pos = True
                 cur_num = 0
             elif char == '-':
-                if cur_num != 0:
-                    new_stack.append(cur_num)
-                new_stack.append('-')
+                overall_num = self.combine(overall_num, cur_num, pos)
+                pos = False
                 cur_num = 0
             elif char == ')':
-                if cur_num != 0:
-                    new_stack.append(cur_num)
+                overall_num = self.combine(overall_num, cur_num, pos)
                 cur_num = 0
                 break
             idx += 1
         if cur_num != 0:
-            new_stack.append(cur_num)
-        # print(new_stack)
-        ans = 0
-        pos = True
-        for val in new_stack:
-            if val == '+':
-                pos = True
-            elif val == '-':
-                pos = False
-            else:
-                if pos:
-                    ans += val
-                else:
-                    ans -= val
-        stack.append(ans)
-        return idx                
+            overall_num = self.combine(overall_num, cur_num, pos)
+        return [overall_num, idx]                
          
     def calculate(self, s: str) -> int:
-        stack = []
-        self.in_paren(stack, s, 0)
-        return stack[0]
+        return self.in_paren(s, 0)[0]
                 
