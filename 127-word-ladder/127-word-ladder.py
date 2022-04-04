ece9ca9d23aebe1727dae046fc7e8e390ff7b164
NeejWeej <nijat.khanbabayev@gmail.com>
2022-04-04 05:23:48 -0400

    Time: 1912 ms (5.00%), Space: 15.2 MB (60.41%) - LeetHub

--- 127-word-ladder/127-word-ladder.py
+++ 127-word-ladder/127-word-ladder.py
@@ -12,24 +12,31 @@ class Solution:
         return False
     
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        graph = collections.defaultdict(set)
         # n = len(wordList)
         wl = len(wordList[0])
-        q = collections.deque([(beginWord, 1)])
-        # visited = set([beginWord])
+        q = set([beginWord])
+        backwards = set([endWord])
+        d = 1
         unvisited = set(wordList)
+        b_unv = set(wordList)
         if endWord not in unvisited:
             return 0
-        while q:
-            cur, val = q.popleft()[:]
-            if cur == endWord:
-                return val
-            remove = []
-            for word in unvisited:
-                if self.wordCompare(cur, word, wl):
-                    remove.append(word)
-            for word in remove:
-                unvisited.remove(word)
-                q.append((word, val + 1))
+        while q and backwards:
+            # print(q, backwards, d)
+            if len(q) > len(backwards):
+                q, backwards, unvisited, b_unv = backwards, q, b_unv, unvisited
+            next_layer = set()
+            for cur in q:
+                remove = []
+                for word in unvisited:
+                    if self.wordCompare(cur, word, wl):
+                        if word in backwards:
+                            return d + 1
+                        remove.append(word)
+                for word in remove:
+                    unvisited.remove(word)
+                    next_layer.add(word)
+            q = next_layer
+            d += 1
         return 0
                     
