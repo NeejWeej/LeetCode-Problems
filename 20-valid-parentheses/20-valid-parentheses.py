3d9e3eb55de92a16e7863c85ecbfb2e6e15dbf11
NeejWeej <nijat.khanbabayev@gmail.com>
2022-04-07 01:14:19 -0400

    Time: 41 ms (62.32%), Space: 13.9 MB (74.58%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        c2o = {'}': '{', ']': '[', ')': '('}
+        stack = []
+        for c in s:
+            if c not in c2o:
+                stack.append(c)
+            else:
+                if stack and stack[-1] == c2o.get(c):
+                    stack.pop()
+                else:
+                    return False
+        return len(stack) == 0
