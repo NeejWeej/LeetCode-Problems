082545868df0d62918c5c70081b8a813c063cdc3
NeejWeej <nijat.khanbabayev@gmail.com>
2022-04-14 04:36:27 -0400

    Time: 76 ms (83.64%), Space: 18.5 MB (96.08%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -36,21 +36,21 @@ class Solution:
             if cur.right:
                 stack.append((cur.right, h + 1))
             # print([x.val for x in stack], [x.val for x in path])
-        path = [x for x,y in path]
+        # path = [x for x,y in path]
         node = q
-        if path[-1].val == q.val:
+        if path[-1][0].val == q.val:
             node = p
         idx = len(path) - 1
-        if search(path[idx].left, node) or search(path[idx].right, node):
-            return path[idx]
+        if search(path[idx][0].left, node) or search(path[idx][0].right, node):
+            return path[idx][0]
         idx -= 1
         while idx >= 0:
-            if path[idx].left and path[idx].left.val != path[idx + 1].val:
-                if search(path[idx].left, node):
-                    return path[idx]
-            elif path[idx].right and path[idx].right.val != path[idx + 1].val:
-                if search(path[idx].right, node):
-                    return path[idx]
+            if path[idx][0].left and path[idx][0].left.val != path[idx + 1][0].val:
+                if search(path[idx][0].left, node):
+                    return path[idx][0]
+            elif path[idx][0].right and path[idx][0].right.val != path[idx + 1][0].val:
+                if search(path[idx][0].right, node):
+                    return path[idx][0]
             idx -= 1         
         
                     
