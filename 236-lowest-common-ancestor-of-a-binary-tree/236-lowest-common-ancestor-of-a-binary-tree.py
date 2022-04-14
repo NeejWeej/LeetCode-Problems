6eee05492260058f2f1dbddd09de91376274be2a
NeejWeej <nijat.khanbabayev@gmail.com>
2022-04-14 04:18:43 -0400

    Time: 77 ms (79.85%), Space: 18.4 MB (96.92%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -21,21 +21,22 @@ class Solution:
                     stack.append(cur.right)
             return False
                   
-        stack = [root]
+        stack = [(root, 0)]
         path = []
         cur = root
         while stack:
-            cur = stack.pop()
-            path.append(cur)
+            cur, h = stack.pop()
+            while path and path[-1][-1] >= h:
+                path.pop()
+            path.append((cur, h))
             if cur.val == p.val or cur.val == q.val:
                 break
             if cur.left:
-                stack.append(cur.left)
+                stack.append((cur.left, h + 1))
             if cur.right:
-                stack.append(cur.right)
-            if not cur.left and not cur.right:
-                path.pop()
+                stack.append((cur.right, h + 1))
             # print([x.val for x in stack], [x.val for x in path])
+        path = [x for x,y in path]
         node = q
         if path[-1].val == q.val:
             node = p
