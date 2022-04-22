632dbe3772057ae1fb9a25a4d7ada6d87940c213
NeejWeej <nijat.khanbabayev@gmail.com>
2022-04-22 15:19:10 -0400

    Time: 118 ms (5.34%), Space: 13.9 MB (49.30%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -8,7 +8,7 @@ class Solution:
             for c in range(cols):
                 if grid[r][c] == 2:
                     new_rotting.append((r,c))
-        minutes = 0
+        minutes = -1
         while new_rotting:
             next_rotting = []
             for r, c in new_rotting:
@@ -17,12 +17,12 @@ class Solution:
                         grid[r + dx][c + dy] = 2
                         next_rotting.append((r + dx,c + dy))
             # print(new_rotting, next_rotting)
-            if not next_rotting:
-                break
+            # if not next_rotting:
+            #     break
             minutes += 1
             new_rotting = next_rotting
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
                     return -1
-        return minutes
+        return max(minutes, 0)
