1d64b18b445ee9a6367ba4ea70290d87adf44b5b
NeejWeej <nijat.khanbabayev@gmail.com>
2022-04-22 15:57:49 -0400

    Time: 56 ms (62.22%), Space: 15.6 MB (99.52%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
@@ -14,7 +14,32 @@ class Solution:
             self.longest_path = left + right
         return max(left, right) + 1
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        self.longest_path = 0
-        self.getHeight(root)
-        return self.longest_path
+        # self.longest_path = 0
+        # self.getHeight(root)
+        # return self.longest_path
+    
+        lp = 0
+        stack = [[root, 0]]
+        h = {}
+        while stack:
+            # print([(x[0].val, x[1]) for x in stack])
+            node, seen = stack[-1]
+            if seen == 0:
+                stack[-1][1] = 1
+                if node.left:
+                    stack.append([node.left, 0])
+            elif seen == 1:
+                stack[-1][1] = 2
+                if node.right:
+                    stack.append([node.right, 0])
+            elif seen == 2:
+                left = h.get(node.left, 0)
+                right = h.get(node.right, 0)
+                if left + right > lp:
+                    lp = left + right
+                h[node] = max(left, right) + 1
+                stack.pop()
+        return lp
+        
+                 
             
