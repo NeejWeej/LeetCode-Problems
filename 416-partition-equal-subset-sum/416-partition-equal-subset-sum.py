97e3bcb657239581ca284cbf056486d12166f15e
NeejWeej <nijat.khanbabayev@gmail.com>
2022-04-26 16:13:16 -0400

    Time: 385 ms (83.14%), Space: 15.4 MB (62.77%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -1,20 +1,22 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        total = sum(nums)
-        n = len(nums)
-        if total % 2 == 1: return False
-        target = total // 2
-        dp = [set() for _ in range(n)]
-        dp[-1].add(nums[-1])
-        dp[-1].add(0)
-        for i in range(n - 2, -1, -1):
-            cur = nums[i]
-            for num in dp[i + 1]:
-                if num + cur <= target:
-                    dp[i].add(num + cur)
-                    dp[i].add(num)
-                elif num <= target:
-                    dp[i].add(num)
-        return target in dp[0]
+        # speeds up the program
+        nums.sort(reverse=True)
+        # if sum of elements cant be divided equally
+        if sum(nums) % 2: return False
+        target = sum(nums) // 2
+        possible_sums = set([0])
+
+        for el in nums:
+            # using a temporary holder to not update possible_sums during cycle run
+            next_sums = set()
+            for s in possible_sums:
+                # if target sum is found, we may assume the other "half" is contained in 
+                # the remaining elemnts of nums
+                if s + el == target:
+                    return True
+                next_sums.add(s+el)
+            possible_sums.update(next_sums)
+        return False
                 
             
