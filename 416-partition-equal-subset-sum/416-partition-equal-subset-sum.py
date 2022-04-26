dfcbb2964f15a3b907c09b11bda2a578bf0b33b2
NeejWeej <nijat.khanbabayev@gmail.com>
2022-04-26 16:08:19 -0400

    Time: 1457 ms (50.86%), Space: 52.8 MB (15.92%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -3,14 +3,18 @@ class Solution:
         total = sum(nums)
         n = len(nums)
         if total % 2 == 1: return False
+        target = total // 2
         dp = [set() for _ in range(n)]
         dp[-1].add(nums[-1])
-        dp[-1].add(-nums[-1])
+        dp[-1].add(0)
         for i in range(n - 2, -1, -1):
             cur = nums[i]
             for num in dp[i + 1]:
-                dp[i].add(num + cur)
-                dp[i].add(num - cur)
-        return 0 in dp[0]
+                if num + cur <= target:
+                    dp[i].add(num + cur)
+                    dp[i].add(num)
+                elif num <= target:
+                    dp[i].add(num)
+        return target in dp[0]
                 
             
