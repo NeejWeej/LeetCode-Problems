0bd05bf2b5b144653beec791a1a8809b0886d83b
NeejWeej <nijat.khanbabayev@gmail.com>
2022-05-10 05:59:24 -0400

    Time: 314 ms (42.39%), Space: 17.7 MB (97.04%) - LeetHub

---
+++ 721-accounts-merge/721-accounts-merge.py
@@ -0,0 +1,45 @@
+from collections import defaultdict
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        def get_parent(parents, start):
+            # print('first '+start)
+            while parents.get(start):
+                start = parents[start]
+                # print(start)
+            return start
+        accs = {}
+        acc_to_name = {}
+        for name, *account in accounts:
+            seen_person = False
+            seen_a = set()
+            for a in account:
+                if a in accs:
+                    seen_person = True
+                    seen_a.add(a)
+            if not seen_person:
+                first = account[0]
+                accs[first] = None
+                acc_to_name[first] = name
+                for acc in account:
+                    if acc != first:
+                        accs[acc] = first
+            elif seen_person:
+                first = next(iter(seen_a))
+                p = get_parent(accs, first)
+                for a in seen_a:
+                    parent = get_parent(accs, a)
+                    if parent != p:
+                        accs[parent] = p
+                for a in account:
+                    if a != p:
+                        accs[a] = p
+        set_items = defaultdict(list)
+        for a in accs:
+            p = get_parent(accs, a)
+            set_items[p].append(a)
+        ans = [
+            [acc_to_name.get(acc)] + sorted(it) 
+            for acc,it in set_items.items()
+            ]   
+        # print(accs)
+        return ans
