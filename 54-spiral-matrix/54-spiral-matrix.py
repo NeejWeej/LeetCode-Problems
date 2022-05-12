ff82b0a6bd4f32e7119d38e79724365004e75867
NeejWeej <nijat.khanbabayev@gmail.com>
2022-05-11 23:12:20 -0400

    Time: 44 ms (41.84%), Space: 14 MB (33.86%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -1,8 +1,8 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         m = len(matrix)
-        if m == 1:
-            return [x for x in matrix[0]]
+        # if m == 1:
+        #     return [x for x in matrix[0]]
         n = len(matrix[0])
         if n == 1:
             return [matrix[i][0] for i in range(m)]
