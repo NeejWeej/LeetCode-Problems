e549abd5a1937baf42a330726729cb401bbd98b3
NeejWeej <nijat.khanbabayev@gmail.com>
2022-05-14 00:53:19 -0400

    Time: 1000 ms (24.34%), Space: 29.5 MB (48.11%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -8,31 +8,24 @@ class Solution:
         jobs.sort(key = lambda x: x[1])
         def binSearch(jobs, i, start):
             s = 0
-            e = i - 1
-            while s + 1 < e:
+            e = i
+            while s < e:
                 mid = s + (e - s)//2
                 if jobs[mid][0] <= start:
-                    s = mid
-                elif jobs[mid][0] > start:
-                    e = mid - 1        
-            if jobs[e][0] > start:
-                return s
-            return e
+                    s = mid + 1
+                else:
+                    e = mid        
+            return s
                 
         dp = [[0,0] for i in range(n)]
         dp[0] = [jobs[0][1], jobs[0][2]]
         for i in range(1, n):
             start, end, profit = jobs[i]
-            #last one that ends before start
-            if start < dp[0][0]:
-                if dp[i - 1][1] >= profit:
-                    dp[i] = dp[i-1][:]
-                else:
-                    dp[i] = [end, profit]
-                continue
-            e = binSearch(dp, i, start)
-            if dp[e][1] + profit > dp[i - 1][1]:
-                dp[i] = [end, dp[e][1] + profit]
+            #first one that ends strictly after start
+            e = binSearch(dp, i, start) - 1
+            compatible_best = dp[e][1] if e != -1 else 0 
+            if compatible_best + profit > dp[i - 1][1]:
+                dp[i] = [end, compatible_best + profit]
             else:
                 dp[i] = dp[i - 1][:]
         return dp[-1][1]
