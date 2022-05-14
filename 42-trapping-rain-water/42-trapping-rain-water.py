7450b8bfc435201837de599f155b757e481b8e9c
NeejWeej <nijat.khanbabayev@gmail.com>
2022-05-14 04:27:51 -0400

    Time: 102 ms (88.53%), Space: 15.8 MB (60.78%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,29 +1,29 @@
 class Solution:
+    from itertools import islice
     def trap(self, height: List[int]) -> int:
-        start = 0
-        elev = height[start]
+        stack = []
+        start = None
         ans = 0
-        greatest = [float('-inf')]
-        n = len(height)
-        cur_max = height[-1]
-        for h in range(n - 2, -1, -1):
-            greatest.append(cur_max)
-            cur_max = max(cur_max, height[h])    
-        i = 1
-        while i < n and start < n:
-            if i < start:
-                i = start + 1
-            if i == n:
-                break
-            elev = min(height[start], greatest[-start - 1])
-            cur = height[i]
-            diff = elev - cur
-            if diff > 0:
-                ans += diff
+        max_afterwards = [-1]
+        for h in reversed(height):
+            max_afterwards.append(max(h, max_afterwards[-1]))
+        max_afterwards = max_afterwards[::-1]
+        def popStack(start, stack, ans):
+            while stack:
+                s = stack.pop()
+                ans += start - s
+            return ans
+        # print(max_afterwards)
+        for i, h in enumerate(height):
+            if not start:
+                start = min(max_afterwards[i + 1], h)
+            elif h >= start:
+                ans = popStack(start, stack, ans)
+                start = min(max_afterwards[i + 1], h)
             else:
-                start = i
-            i += 1
+                stack.append(h)  
         return ans
+            
             
             
                 
