2d698e53b7e8c3251f9f7301bbd07a36720e5e24
NeejWeej <nijat.khanbabayev@gmail.com>
2022-05-25 01:57:19 -0400

    Time: 576 ms (70.11%), Space: 27.6 MB (10.43%) - LeetHub

--- 310-minimum-height-trees/310-minimum-height-trees.py
+++ 310-minimum-height-trees/310-minimum-height-trees.py
@@ -13,17 +13,19 @@ class Solution:
         layer = start
         while layer:
             nl = set()
-            if len(layer) == 1:
-                return layer
+            # if len(layer) == 1:
+            #     return layer
             for x in layer:
                 while e[x]:
                     y = e[x].pop()
-                    if y in layer:
-                        return layer
+                    # if y in layer:
+                    #     return layer
                     e[y].remove(x)
                     if len(e[y]) == 1:
                         nl.add(y)
                 del e[x]
+            if len(nl) == 0:
+                return layer
             layer = nl
             
             
