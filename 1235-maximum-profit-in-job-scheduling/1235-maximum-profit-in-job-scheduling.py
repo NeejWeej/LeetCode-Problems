99e709f5786585ed9536d1d5eade00d62e2396e3
NeejWeej <nijat.khanbabayev@gmail.com>
2022-06-19 16:39:04 -0400

    Time: 1023 ms (26.84%), Space: 32.9 MB (44.46%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -1,31 +1,43 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        def leftMost(jobs, curJ, endingIdx):
+            startT = curJ[0]
+            #find first idx where the endtime is strictly greater than our start time
+            start = 0
+            end = endingIdx
+            while start < end:
+                mid = start + (end - start)//2
+                #if endtime is strictly greater, we have this as our end
+                if startT < jobs[mid][0]:
+                    end = mid
+                elif startT >= jobs[mid][0]:
+                    start = mid + 1
+                # if startT <= jobs[mid][0]:
+                #     start = mid + 1
+                # elif startT > jobs[mid][0]:
+                #     end = mid
+            return start
+        times = [(start, end, p) for start,end,p in zip(startTime, endTime, profit)]
+        times.sort(key = lambda x: x[1])
         n = len(startTime)
-        jobs = [
-            (startTime[i], endTime[i], profit[i])
-            for i in range(n)
-        ]
-        jobs.sort(key = lambda x: x[1])
-        def binSearch(jobs, i, start):
-            s = 0
-            e = i
-            while s < e:
-                mid = s + (e - s)//2
-                if jobs[mid][0] <= start:
-                    s = mid + 1
-                else:
-                    e = mid        
-            return s
-                
-        dp = [[0,0] for i in range(n)]
-        dp[0] = [jobs[0][1], jobs[0][2]]
+        #each element of dp is [endTime, profit]
+        dp = [[0, 0] for _ in range(n)]
+        dp[0] = times[0][1:]
+        # print(dp)
         for i in range(1, n):
-            start, end, profit = jobs[i]
-            #first one that ends strictly after start
-            e = binSearch(dp, i, start) - 1
-            compatible_best = dp[e][1] if e >= 0 else 0 
-            if compatible_best + profit > dp[i - 1][1]:
-                dp[i] = [end, compatible_best + profit]
+            curJ = times[i]
+            if curJ[0] >= dp[i-1][0]:
+                #if start time is after all end times:
+                dp[i] = [curJ[1], dp[i-1][1] + curJ[2]]
+                continue
+            leftI = leftMost(dp, curJ, i-1)
+            bestWith = curJ[2]
+            if leftI > 0:
+                bestWith += dp[leftI - 1][1]
+            if bestWith > dp[i-1][1]:
+                dp[i] = [curJ[1], bestWith]
             else:
-                dp[i] = dp[i - 1][:]
+                # dp[i] = [dp[i-1][0], dp[i-1][1]]
+                dp[i] = dp[i-1][:]   
+        print(dp)
         return dp[-1][1]
