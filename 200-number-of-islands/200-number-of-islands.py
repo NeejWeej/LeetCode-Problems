9d7b94ec80a7a970225dd4756dc0a67754524b96
NeejWeej <nijat.khanbabayev@gmail.com>
2022-06-27 00:44:24 -0400

    Time: 486 ms (42.31%), Space: 16.3 MB (79.64%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,18 +1,19 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
         def search(grid, x, y):
+            grid[x][y] = "0"
             direc = [(0, 1), (0, -1), (-1, 0), (1, 0)]
             for dx, dy in direc:
-                if 0<=x + dx < len(grid) and 0<=y+dy<len(grid[0]):
+                if 0<= x + dx < m and 0<= y+dy < n:
                     if grid[x+dx][y+dy] == "1":
-                        grid[x+dx][y+dy] = -1
                         search(grid, x+dx, y + dy)
         ans = 0
-        for m in range(len(grid)):
-            for n in range(len(grid[0])):
-                if grid[m][n] == "1":
-                    grid[m][n] = -1
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1":
                     ans += 1
-                    search(grid, m, n)
+                    search(grid, i, j)
         return ans
         
