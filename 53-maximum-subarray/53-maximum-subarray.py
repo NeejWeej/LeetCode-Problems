a3e23ec4e5fdd5bc54b27561dc3840a999d075f6
NeejWeej <nijat.khanbabayev@gmail.com>
2022-07-02 21:04:08 -0400

    Time: 1340 ms (19.59%), Space: 28.8 MB (9.62%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,8 +1,18 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         cur = best = float('-inf')
+        curStart = -1
+        bestS, bestE = -1, -1
         for idx, num in enumerate(nums):
-            cur = max(cur + num, num)
-            best = max(best, cur)
+            if cur + num < num:
+                curStart = idx
+                cur = num
+            else:
+                cur += num
+            if best < cur:
+                bestS = curStart
+                bestE = idx
+                best = cur
+        print(nums[bestS: bestE + 1])
         return best
         
