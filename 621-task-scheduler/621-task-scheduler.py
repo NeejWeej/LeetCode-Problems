6e321475c3ed0e2439e5271b60a89a41441f5d49
NeejWeej <nijat.khanbabayev@gmail.com>
2022-07-17 16:08:57 -0400

    Time: 608 ms (77.36%), Space: 14.3 MB (89.93%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.py
+++ 621-task-scheduler/621-task-scheduler.py
@@ -4,12 +4,9 @@ class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         if n == 0:
             return len(tasks)
-        counter = defaultdict(int)
-        best = 0
-        for t in tasks:
-            counter[t] += 1
-            if counter[t] > best:
-                best = counter[t]
+        counter = Counter(tasks)
+        top = counter.most_common(1)[0]
+        best = top[1]
         best_count = 0
         for k,v in counter.items():
             if v == best:
