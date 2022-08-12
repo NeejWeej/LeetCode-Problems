32dfcec34c83cc0630d509fff5c55d475d2c0038
NeejWeej <nijat.khanbabayev@gmail.com>
2022-08-12 16:45:58 -0400

    Time: 1701 ms (79.10%), Space: 14.4 MB (42.47%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,29 +1,14 @@
 class Solution:
-    import random
     def coinChange(self, coins: List[int], amount: int) -> int:
         if amount == 0:
             return 0
-        seen = set()
-        q = [amount]
-        count = 0
-        while q:
-            count += 1
-            new_layer = []
-            random.shuffle(coins)
-            for cur in q:
-                for coin in coins:
-                    if cur - coin == 0:
-                        return count
-
-                    if cur - coin > 0 and cur - coin not in seen:
-                        seen.add(cur - coin)
-                        new_layer.append(cur - coin)
-                        continue
-            q = new_layer
-        return -1
+        vals = [float('inf') for _ in range(amount + 1)]
+        for c in coins:
+            if c > amount:
+                continue
                 
-                    
-                    
-            
-            
+            vals[c] = 1
+            for i in range(c + 1, amount + 1):
+                vals[i] = min(vals[i - c] + 1, vals[i])
+        return vals[amount] if vals[amount] != float('inf') else -1
         
