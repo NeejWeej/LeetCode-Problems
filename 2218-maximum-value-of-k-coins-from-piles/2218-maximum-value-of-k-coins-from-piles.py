e89aa9e00c061bc2b8922511cafffdf7b29afc67
NeejWeej <nijat.khanbabayev@gmail.com>
2022-08-15 17:19:14 -0400

    Time: 7829 ms (40.63%), Space: 53.9 MB (77.74%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.py
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.py
@@ -1,26 +1,23 @@
 class Solution:
     def maxValueOfCoins(self, piles: List[List[int]], k: int) -> int:
         n = len(piles)
-        
-        
-#         def pfSum(pile, length):
-#             res = [0]
-#             for p in pile:
-#                 res.append(res[-1])
-#                 res[-1] += p
-#             return res
-        
-#         dpPiles = [pfSum(p, k + 1) for p in piles]
-        
-#         dp = [dpPiles[0] + [float('-inf') for _ in range(len(dpPiles[0]), k + 1)]] + \
-#         [[float('-inf') for _ in range(k + 1)] for _ in range(n)]
-        
         dp = [[0 for _ in range(k + 1)] for _ in range(n)]
-        start = 0
-        for idx, val in enumerate(piles[0]):
-            if idx == len(dp[0]) - 1:
-                break
-            dp[0][idx + 1] = dp[0][idx] + val
+        
+        val = 0
+        firstPile = piles[0]
+        firstDP = dp[0]
+        
+        for i in range(min(k + 1, len(firstPile))):
+            firstDP[i] = val
+            val += firstPile[i]
+            
+        if len(firstPile) < k + 1:
+            firstDP[len(firstPile)] = val
+            
+        # for idx, val in enumerate(piles[0]):
+        #     if idx == len(dp[0]) - 1:
+        #         break
+        #     dp[0][idx + 1] = dp[0][idx] + val
         
         for i in range(1, n):
             pileSize = len(piles[i])
@@ -29,12 +26,12 @@ class Solution:
                 best = 0
                 cur = 0
                 for inPile in range(min(pileSize, coins + 1)):
-                    # cur = dpPiles[i][inPile]
                     last = dp[i-1][coins-inPile]
                     best = max(best, last + cur)
                     cur += piles[i][inPile]
                     
                 if pileSize < coins + 1:
+                    # we ended early, have to include final coin added
                     last = dp[i-1][coins - inPile - 1]
                     best = max(best, last + cur) 
                     
