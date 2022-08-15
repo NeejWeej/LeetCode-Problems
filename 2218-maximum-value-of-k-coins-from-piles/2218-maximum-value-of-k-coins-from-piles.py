3137a265cac3e208edd9f2c043621a3e0c22309e
NeejWeej <nijat.khanbabayev@gmail.com>
2022-08-15 16:56:45 -0400

    Time: 9140 ms (12.51%), Space: 54.6 MB (76.17%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.py
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.py
@@ -1,27 +1,18 @@
 class Solution:
     def maxValueOfCoins(self, piles: List[List[int]], k: int) -> int:
-        self.ans = 0
         n = len(piles)
-        totalCoins = sum(len(pile) for pile in piles)
         
         def pfSum(pile, length):
             res = [0]
             for p in pile:
                 res.append(res[-1])
                 res[-1] += p
-            
-            # # invalidate ones we can never access
-            # for i in range(len(res), length):
-            #     res.append(float('-inf'))
             return res
         
         dpPiles = [pfSum(p, k + 1) for p in piles]
         
-        dp = [dpPiles[0] + [0 for _ in range(len(dpPiles[0]), k + 1)]] + [[float('-inf') for _ in range(k + 1)] for _ in range(n)]
-            
-        # dp = [[0 for i in range(k + 1)] for _ in range(n)]
-        #max amount for [0, k] using first idx - 1 piles
-        # dp[0] = dpPiles[0]
+        dp = [dpPiles[0] + [float('-inf') for _ in range(len(dpPiles[0]), k + 1)]] + \
+        [[float('-inf') for _ in range(k + 1)] for _ in range(n)]
         
         for i in range(1, n):
             pileSize = len(dpPiles[i])
