753a12c2f76e6d5f566e58b06eaf2f62406f2ae6
NeejWeej <nijat.khanbabayev@gmail.com>
2022-08-17 14:18:58 -0400

    Time: 53 ms (35.22%), Space: 13.9 MB (83.39%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -1,38 +1,43 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        row = 0
-        col = 0
         m = len(matrix)
         n = len(matrix[0])
-        topRowSeen = -1
-        botRowSeen = m
-        leftColSeen = -1
-        rightColSeen = n
+        top = left = 0
+        bot = m - 1
+        right = n - 1
         
-        res = [matrix[0][0]]
+        res = []
+        def matrixDone(top, bot, left, right):
+            return  top > bot or left > right
+        
+        while True:
+            for i in range(left, right + 1):
+                res.append(matrix[top][i])
+            top += 1
+            if matrixDone(top, bot, left, right):
+                return res
+                
+            for i in range(top, bot + 1):
+                res.append(matrix[i][right])  
+            right -= 1
+            if matrixDone(top, bot, left, right):
+                return res
+                
+            for i in range(right, left - 1, -1):
+                res.append(matrix[bot][i])   
+            bot -= 1
+            if matrixDone(top, bot, left, right):
+                return res
             
-        while botRowSeen != topRowSeen or \
-        leftColSeen != rightColSeen:
-            for idx, (dx, dy) in enumerate(directions):
-                row += dx
-                col += dy
-                while botRowSeen > row > topRowSeen and \
-                leftColSeen < col < rightColSeen:
-                    res.append(matrix[row][col])
-                    row += dx
-                    col += dy
-                row -= dx
-                col -= dy
-                if idx == 0: 
-                    topRowSeen = max(row, topRowSeen)
-                elif idx == 1:
-                    rightColSeen = min(col, rightColSeen)
-                elif idx == 2:
-                    botRowSeen = min(row, botRowSeen)
-                elif idx == 3:
-                    leftColSeen = max(col, leftColSeen) 
+            for i in range(bot, top - 1, -1):
+                res.append(matrix[i][left])
+            left += 1
+            
+            if matrixDone(top, bot, left, right):
+                return res
+            
+            
         
-        return res
                 
         
