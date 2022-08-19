e28a2158d524ce5691a642a6a8fd006dbbc96abc
NeejWeej <nijat.khanbabayev@gmail.com>
2022-08-18 23:47:06 -0400

    Time: 1214 ms (5.03%), Space: 19 MB (11.31%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.py
+++ 110-balanced-binary-tree/110-balanced-binary-tree.py
@@ -6,29 +6,17 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        if not root:
-            return True
-        dummy = TreeNode()
-        stack = [[dummy] + [0 for _ in range(5)],[root, 0, False, 0, 0]]
-        while len(stack) > 1:
-            cur, kidsSeen, is_left, left_height, right_height = stack[-1]
-            if kidsSeen == 2:
-                if left_height < right_height:
-                    left_height, right_height = right_height, left_height
-                if left_height - right_height > 1:
-                    return False
-                height = left_height + 1
-                if is_left:
-                    stack[-2][3] = height
-                else:
-                    stack[-2][4] = height
-                stack.pop()
-            elif kidsSeen == 0:
-                stack[-1][1] = 1
-                if cur.left:
-                    stack.append([cur.left, 0, True, 0, 0])
-            elif kidsSeen == 1:
-                stack[-1][1] = 2
-                if cur.right:
-                    stack.append([cur.right, 0, False, 0, 0])
-        return True  
+        self.ans = True
+        
+        def height(node):
+            if not node or not self.ans:
+                return 0
+            left = height(node.left)
+            right = height(node.right)
+            if abs(left - right) > 1:
+                self.ans = False
+                return 0
+            return 1 + max(height(node.left), height(node.right))
+        height(root)
+        return self.ans
+        
