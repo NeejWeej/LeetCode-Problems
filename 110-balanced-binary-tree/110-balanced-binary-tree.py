0fa637b20569aa6555c2d992cf50b9577b14cacf
NeejWeej <nijat.khanbabayev@gmail.com>
2022-08-19 00:07:17 -0400

    Time: 62 ms (82.94%), Space: 15.2 MB (98.66%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.py
+++ 110-balanced-binary-tree/110-balanced-binary-tree.py
@@ -4,21 +4,43 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+class HeightNode:
+    def __init__(self, node, isLeft):
+        self.node = node
+        self.isLeft = isLeft
+        self.status = 2
+        self.leftH = 0
+        self.rightH = 0
+        
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        self.ans = True
-        
-        def height(node):
-            if not node or not self.ans:
-                return 0
-            left = height(node.left)
-            if not self.ans:
-                return 0
-            right = height(node.right)
-            if abs(left - right) > 1:
-                self.ans = False
-                return 0
-            return 1 + max(height(node.left), height(node.right))
-        height(root)
-        return self.ans
+        if not root: return True
+        stack = [HeightNode(root, True)]
+        while stack:
+            last = stack[-1]
+            if last.status == 2:
+                last.status -= 1
+                if last.node.left:
+                    stack.append(HeightNode(last.node.left, True))
+                    
+            elif last.status == 1:
+                last.status -= 1
+                if last.node.right:
+                    stack.append(HeightNode(last.node.right, False))
+                    
+            else:
+                if abs(last.leftH - last.rightH) > 1:
+                    return False
+                height = 1 + max(last.leftH, last.rightH)
+                stack.pop()
+                if not stack:
+                    return True
+                if last.isLeft:
+                    stack[-1].leftH = height  
+                else:
+                    stack[-1].rightH = height
+        return True
+                
+            
+            
         
