a0ec56a8a1394ff04c1c05da9d1d490c46330f91
NeejWeej <nijat.khanbabayev@gmail.com>
2022-08-26 17:56:59 -0400

    Time: 137 ms (90.33%), Space: 14.7 MB (91.19%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -3,32 +3,33 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        cols_seen = set()
-        rows_seen = set()
-        rows = len(matrix)
-        cols = len(matrix[0])
+        m = len(matrix)
+        n = len(matrix[0])
         
-        def change_zeroes_col(col):
-            for r in range(rows):
-                matrix[r][col] = 0
+        def setZero(row, col):
+            for i in range(m):
+                if matrix[i][col] != 'e':
+                    matrix[i][col] = 0
+                    
+            for j in range(n):
+                if matrix[row][j] != 'e':
+                    matrix[row][j] = 0
+                
         
-        def change_zeroes_row(row):
-            for c in range(cols):
-                matrix[row][c] = 0
-            
-        for r in range(rows):
-            row_seen = False
-            for c in range(cols):
-                if c in cols_seen and row_seen:
-                    continue
-                if matrix[r][c] == 0:
-                    cols_seen.add(c)
-                    row_seen = True
-            if row_seen:
-                rows_seen.add(r)
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    matrix[i][j] = 'e'
         
-        for r in rows_seen:
-            change_zeroes_row(r)
-        for c in cols_seen:
-            change_zeroes_col(c)
-        return
+                
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 'e':
+                    setZero(i, j)
+        
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 'e':
+                    matrix[i][j] = 0
+                    
+        
