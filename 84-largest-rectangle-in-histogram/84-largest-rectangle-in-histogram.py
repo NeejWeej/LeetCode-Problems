611b93744be6607519f13c29973e2fadba3d79c6
NeejWeej <nijat.khanbabayev@gmail.com>
2022-08-27 12:51:52 -0400

    Time: 2639 ms (5.02%), Space: 35.9 MB (5.03%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
@@ -1,29 +1,29 @@
 class Solution:
-    from collections import deque
     def largestRectangleArea(self, heights: List[int]) -> int:
-        deq = deque([])
+        stack = []
         n = len(heights)
         rightExtend = [0] * n
-        for i,v in enumerate(heights):
-            while len(deq) > 0 and v < deq[-1][1]:
-                idx, lastVal = deq.pop()
-                rightExtend[idx] = i - idx
-            deq.append((i, v))
         
-        while len(deq) > 0:
-            idx, lastVal = deq.pop()
+        for i,v in enumerate(heights):
+            while stack and v < stack[-1][1]:
+                idx, lastVal = stack.pop()
+                rightExtend[idx] = i - idx
+            stack.append((i, v))
+        
+        while stack:
+            idx, lastVal = stack.pop()
             rightExtend[idx] = n - idx 
             
         leftExtend = [0] * n
         
         for i,v in enumerate(reversed(heights)):
-            while len(deq) > 0 and v < deq[-1][1]:
-                idx, lastVal = deq.pop()
+            while stack and v < stack[-1][1]:
+                idx, lastVal = stack.pop()
                 leftExtend[idx] = i - idx
-            deq.append((i, v))
+            stack.append((i, v))
         
-        while len(deq) > 0:
-            idx, lastVal = deq.pop()
+        while stack:
+            idx, lastVal = stack.pop()
             leftExtend[idx] = n - idx 
             
         vals = [h*(leftExtend[-i-1] + rightExtend[i] - 1) for i,h in enumerate(heights)]
