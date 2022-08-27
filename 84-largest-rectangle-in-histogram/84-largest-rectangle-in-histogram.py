a9ad8675768127a1acf9b0c6f0646de6cacff665
NeejWeej <nijat.khanbabayev@gmail.com>
2022-08-27 13:09:19 -0400

    Time: 2122 ms (12.06%), Space: 34.9 MB (5.03%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
@@ -15,16 +15,15 @@ class Solution:
             rightExtend[idx] = n - idx 
         
         ans = 0
-
         for i,v in enumerate(reversed(heights)):
             while stack and v < stack[-1][1]:
                 idx, lastVal = stack.pop()
                 ans = max(ans, lastVal *(i - idx - 1 + rightExtend[-idx-1]))
             stack.append((i,v))
-        
         while stack:
             idx, lastVal = stack.pop()
             ans = max(ans, lastVal * (n - idx - 1 + rightExtend[-idx-1]))
+            
         return ans
             
         
