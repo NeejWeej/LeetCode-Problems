b48a8604950934b5b22329a3e7915152f87cc58e
NeejWeej <nijat.khanbabayev@gmail.com>
2022-08-29 17:09:24 -0400

    Time: 775 ms (92.47%), Space: 72.1 MB (38.63%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.py
+++ 981-time-based-key-value-store/981-time-based-key-value-store.py
@@ -1,33 +1,26 @@
 from collections import defaultdict
+
 class TimeMap:
 
     def __init__(self):
-        self.keys = defaultdict(list)
+        self.d = defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.keys[key].append((timestamp, value))
+        self.d[key].append((timestamp, value))
 
     def get(self, key: str, timestamp: int) -> str:
-        times = self.keys.get(key)
-        if not times:
+        vals = self.d.get(key, [])
+        left, right = 0, len(vals)
+        if not vals or vals[0][0] > timestamp:
             return ""
-        lo = 0
-        hi = len(times) - 1
-        while lo + 1< hi:
-            mid = lo + (hi - lo) // 2
-            mid_t = times[mid]
-            if mid_t[0] == timestamp:
-                return mid_t[1]
-            elif timestamp < mid_t[0]:
-                hi = mid - 1
-            else:   
-                lo = mid
-        if times[hi][0] <= timestamp:
-            return times[hi][1]
-        if times[lo][0] <= timestamp:
-            return times[lo][1]
-        return ""
-            
+        
+        while left < right:
+            mid = (left + right)//2
+            if vals[mid][0] <= timestamp:
+                left = mid + 1  
+            else:
+                right = mid
+        return vals[left - 1][1]
 
 
 # Your TimeMap object will be instantiated and called as such:
