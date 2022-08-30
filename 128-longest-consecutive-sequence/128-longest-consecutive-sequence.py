2fcf8632d9339145b245aec4ecbb3d56b21021ce
NeejWeej <nijat.khanbabayev@gmail.com>
2022-08-30 15:01:57 -0400

    Time: 957 ms (37.83%), Space: 79.9 MB (5.02%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -1,54 +1,17 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
         best = 0
-        set_nums = set(nums)
+        seen = {}
         
-        for num in set_nums:
-            if num - 1 in set_nums:
-                continue
-            cur = 1
-            while num + 1 in set_nums:
-                cur += 1
-                num += 1
-            best = max(cur, best)
+        def storeLongestPath(pathL, num, numSet):
+            if num in pathL or num not in numSet:
+                return pathL.get(num, 0)
+            val = storeLongestPath(pathL, num + 1, numSet)
+            pathL[num] = val + 1
+            return val + 1
+            
+        for num in nums:
+            best = max(best, storeLongestPath(seen, num, numSet))
         return best
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-        # nums.sort()
-        # longest, curr = 1, 1
-        # last_num = nums[0]
-        # direction = 0
-        # for idx,val in enumerate(nums[1:], 1):
-        #     if curr > longest:
-        #         longest = curr
-        #     if val == last_num:
-        #         continue
-        #     if curr == 1:
-        #         if val == last_num + 1:
-        #             curr += 1
-        #             direction = 1
-        #         elif val == last_num - 1:
-        #             curr += 1
-        #             direction = -1
-        #         last_num = val
-        #         continue
-        #     if val == last_num + direction:
-        #         curr += 1
-        #         last_num = val
-        #         continue
-        #     curr = 1
-        #     last_num = val
-        # return max(longest,curr)
-            
-            
         
