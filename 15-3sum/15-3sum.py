223554ab5fea03ca47df3d7e0b3d6972bfb08034
NeejWeej <nijat.khanbabayev@gmail.com>
2022-08-30 23:13:35 -0400

    Time: 3215 ms (23.08%), Space: 17.4 MB (87.32%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,95 +1,23 @@
-class Solution(object):
-    def threeSum(self,nums):
-        n = len(nums)
+class Solution:
+    from itertools import islice
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        cur_idx = 0
-        ans = set()
-        nums_seen = set()
-        while cur_idx < n:
-            val = nums[cur_idx]
-            if val in nums_seen:
-                cur_idx += 1
-                continue
-            start = cur_idx + 1
-            end = n - 1
-            while start < end:
-                while nums[start] in nums_seen:
-                    start += 1
-                while nums[end] in nums_seen:
-                    end -= 1
-                if start >= end:
-                    break
-                triplet_val = nums[start] + nums[end] + val
-                if triplet_val == 0:
-                    ans.add((val, nums[start], nums[end]))
-                    end -= 1
-                    start += 1
-                elif triplet_val < 0:
-                    start += 1
-                elif triplet_val > 0:
-                    end -= 1
-            nums_seen.add(val)
-            cur_idx += 1
-        return ans
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         n = len(nums)
-#         counts = {}
-#         for num in nums:
-#             counts[num] = counts.get(num, 0) + 1
-        
-#         ans = set()
-        
-#         for idx, val in enumerate(nums):
-#             counts[val] -= 1
-#             if counts[val] <= 0:
-#                 counts.pop(val)
-            
-#             for num, count in counts.items():
-#                 if -val - num == num:
-#                     if count > 1:
-#                         new = [num, val, -val-num]
-#                         new.sort()
-#                         ans.add(tuple(new))                    
+        n = len(nums)
+        ans = set([])
+        for idx, num in enumerate(islice(nums, 0, n - 2)):
+            l = idx + 1
+            r = n - 1
+            while l < r:
+                leftPlusRight = nums[l] + nums[r]
+                if leftPlusRight == -num:
+                    ans.add((num, nums[l], nums[r]))
+                    l += 1
+                    r -= 1
+                elif leftPlusRight + num < 0:
+                    l += 1
                     
-#                 elif -val-num in counts:
-#                     new = [num, val, -val-num]
-#                     new.sort()
-#                     ans.add(tuple(new))
-        
-#         return list(ans)
-        
+                else:
+                    # leftPlusRight + num > 0
+                    r -= 1
+        return ans
+                
