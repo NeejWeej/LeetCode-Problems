4554bc6e7ccd1ed06dfde45e3b6813de8378875d
NeejWeej <nijat.khanbabayev@gmail.com>
2022-09-02 17:29:16 -0400

    Time: 206 ms (79.25%), Space: 23.8 MB (47.07%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
@@ -8,18 +8,23 @@ class Solution:
             mid = (l + r) // 2
             left = float('inf') if mid == 0 else nums[mid - 1]
             right = float('inf') if mid == n - 1 else nums[mid + 1]
+            leftEq = left == nums[mid]
+            rightEq = right == nums[mid]
+            
+            if not leftEq and not rightEq:
+                return nums[mid]
             
             if mid % 2 == 0:
                 # should be start
-                if right == nums[mid]:
+                if rightEq:
                     l = mid + 1
                 else:
-                    r = mid
+                    r = mid - 1
             
             else:
-                if left == nums[mid]:
+                if leftEq:
                     l = mid + 1
                 else:
-                    r = mid
+                    r = mid - 1
         return nums[l]
                 
