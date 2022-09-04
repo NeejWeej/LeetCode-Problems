badc742fec5c48b2bf271be97f304964e420af3d
NeejWeej <nijat.khanbabayev@gmail.com>
2022-09-04 16:02:07 -0400

    Time: 254 ms (27.65%), Space: 14.8 MB (36.63%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,29 +1,33 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        m = len(s)
+        n = len(t)
+        if m < n:
+            return ""
+        
+        bestS, bestE = 0, m
+        
         start = 0
-        best = [float('inf'), '']
-        t_counts = {}
-        cur = {}
-        for c in t:
-            t_counts[c] = t_counts.get(c, 0) + 1
-        need = len(t_counts)
-        for i, c in enumerate(s):
-            cur_c = cur.get(c, 0)
-            cur[c] = cur_c + 1
-            if cur_c == t_counts.get(c, 0) - 1:
-                need -= 1
-            if need == 0:
-                start_letter = s[start]
-                start_letter_count = cur.get(start_letter)
-                while start_letter_count > t_counts.get(start_letter, 0):
-                    cur[start_letter] = start_letter_count - 1
+        
+        tCounter = Counter(t)
+        count = len(tCounter)
+        counter = {}
+        
+        for i,v in enumerate(s):
+            counter[v] = counter.get(v, 0) + 1
+            if counter.get(v) == tCounter.get(v):
+                count -= 1
+            
+            if count == 0:
+                while counter.get(s[start], 0) > tCounter.get(s[start], -1):
+                    oldVal = counter.pop(s[start], 0)
+                    if oldVal > 1:
+                        counter[s[start]] = oldVal - 1
                     start += 1
-                    start_letter = s[start]
-                    start_letter_count = cur.get(start_letter)
-                if i - start + 1 < best[0]:
-                    best = [i - start + 1, s[start: i + 1]]
-        return best[1]
-            
                 
-                
-            
+                if bestE - bestS > i - start:
+                    bestE, bestS = i, start
+        
+        return s[bestS: bestE + 1] if bestE < m else ""
+        
+        
