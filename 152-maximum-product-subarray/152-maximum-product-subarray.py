aef4ea386a79b376e8bebfd52b83db3f27497dc8
NeejWeej <nijat.khanbabayev@gmail.com>
2022-09-05 15:42:18 -0400

    Time: 153 ms (32.15%), Space: 14.4 MB (79.54%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -2,8 +2,7 @@ from itertools import islice
 
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        largeDP = smallDP = nums[0]
-        best = nums[0]
+        largeDP = smallDP = best = nums[0]
         for num in islice(nums, 1, len(nums)):
             largeNew = max(num, smallDP * num, largeDP * num)
             smallNew = min(num, smallDP * num, largeDP * num)
