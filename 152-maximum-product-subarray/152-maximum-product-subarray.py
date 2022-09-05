bed156872026451ba4326d681156803328c3aa28
NeejWeej <nijat.khanbabayev@gmail.com>
2022-09-05 16:00:56 -0400

    Time: 218 ms (5.19%), Space: 14.5 MB (18.24%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -3,7 +3,7 @@ from itertools import islice
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         largeDP = smallDP = best = nums[0]
-        for num in islice(nums, 1, len(nums)):
+        for num in (val for i,val in enumerate(nums) if i > 0):
             candidates = sorted([num, smallDP * num, largeDP*num])
             largeDP = candidates[-1]
             smallDP = candidates[0]
