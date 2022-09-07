033e0d1bb540c70174f64505853d82cfb0eae6bb
NeejWeej <nijat.khanbabayev@gmail.com>
2022-09-07 01:21:45 -0400

    Time: 222 ms (67.83%), Space: 17.8 MB (29.56%) - LeetHub

--- 127-word-ladder/127-word-ladder.py
+++ 127-word-ladder/127-word-ladder.py
@@ -1,42 +1,63 @@
-import collections
+from collections import defaultdict, deque
 class Solution:
-    def wordCompare(self, w1, w2, wl):
-        diffs = 0
-        for j in range(wl):
-            if w1[j] != w2[j]:
-                diffs += 1
-                if diffs > 1:
-                    break
-        if diffs == 1:
-            return True
-        return False
-    
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        # n = len(wordList)
-        wl = len(wordList[0])
-        q = set([beginWord])
-        backwards = set([endWord])
-        d = 1
-        unvisited = set(wordList)
-        b_unv = set(wordList)
-        if endWord not in unvisited:
+        # transitions = defaultdict(list)
+        templates = defaultdict(list)
+        wordList.append(beginWord)
+        n = len(wordList)
+        m = len(beginWord)
+        
+        foundEnd = False
+        for word in wordList:
+            if word == endWord:
+                foundEnd = True
+            for i in range(m):
+                templateS = word[:i] + "_" + word[i + 1:]
+                templates[templateS].append(word)
+        if not foundEnd:
             return 0
-        while q and backwards:
-            # print(q, backwards, d)
-            if len(q) > len(backwards):
-                q, backwards, unvisited, b_unv = backwards, q, b_unv, unvisited
-            next_layer = set()
-            for cur in q:
-                remove = []
-                for word in unvisited:
-                    if self.wordCompare(cur, word, wl):
-                        if word in backwards:
-                            return d + 1
-                        remove.append(word)
-                for word in remove:
-                    unvisited.remove(word)
-                    next_layer.add(word)
-            q = next_layer
-            d += 1
-        return 0
-                    
+        # use bfs, if we ever see a word again, ignore cause we already saw best
+        # way to get to it from start
+        
+        # start at 0 since when we first pop we increment
+        startQ = [beginWord]
+        startSeen = {beginWord: 1}
+        endQ = [endWord]
+        endSeen = {endWord:1}
+        # print(transitions)
+        self.ans = float('inf')
+        
+        def moveQ(q, seen, otherSeen):
+            newQ = []
+            for val in q:
+                dist = seen.get(val)
+                if val in otherSeen:
+                    # subtract 1 because we double count since we double count the overlap
+                    self.ans = min(self.ans, dist + otherSeen.get(val) - 1)
+                    return newQ
+                for i in range(m):
+                    tempS = val[:i] + "_" + val[i + 1:]
+                    for word in templates.get(tempS, []):
+                        if word in otherSeen:
+                            self.ans = min(self.ans, dist + otherSeen.get(word))
+                        elif word not in seen:
+                            seen[word] = dist + 1
+                            newQ.append(word)
+            return newQ
+        
+        while startQ and endQ and self.ans == float('inf'):
+            startQ = moveQ(startQ, startSeen, endSeen)
+            if self.ans == float('inf'):
+                endQ = moveQ(endQ, endSeen, startSeen)
+        return self.ans if self.ans != float('inf') else 0
+        
+        
+        
+        
+        
+        
+        
+                        
+                
+        
+        
