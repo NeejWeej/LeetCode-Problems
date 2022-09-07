356c1cd9e1b4ff26d7b8d2a563010ebe59119369
NeejWeej <nijat.khanbabayev@gmail.com>
2022-09-07 01:38:47 -0400

    Time: 7544 ms (52.83%), Space: 13.9 MB (50.74%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,102 +1,28 @@
 class Solution:
-    
-    def dfs(self, board, next_idx, word, row, col, m, n, seen):
-        if next_idx == len(word):
-            return True
-        
-        seen.add((row, col))
-        
-        for n_r, n_c in ((row, 1 + col), (row, col - 1), (row + 1, col), (row - 1, col)):
-            if (0<=n_r<m and 0<=n_c<n) and (n_r, n_c) not in seen \
-                and word[next_idx] == board[n_r][n_c] \
-                and self.dfs(board, next_idx + 1, word, n_r, n_c, m, n, seen):
-                    return True
-        seen.remove((row,col))
-        return False
-                
     def exist(self, board: List[List[str]], word: str) -> bool:
         m = len(board)
         n = len(board[0])
+        wordLen = len(word)
         
-        for r in range(m):
-            for c in range(n):
-                if board[r][c] == word[0] and self.dfs(board, 1, word, r, c, m, n, set()):
+        direc = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        self.ans = False
+        
+        def search(r, c, needIdx):
+            old = board[r][c]
+            board[r][c] = '$'
+            
+            if needIdx == wordLen or self.ans:
+                self.ans = True
+                return
+            for dx, dy in direc:
+                if 0 <= r+dx<m and 0<=c+dy<n and board[r+dx][c+dy] == word[needIdx]:
+                    search(r+dx, c+dy, needIdx + 1)
+            board[r][c] = old
+        
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == word[0]:
+                    search(i, j, 1)
+                    if self.ans:
                         return True
-        return False
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         def search(x, y, cur_word):
-#             cur_letter = board[x][y]
-#             if cur_letter == '*':
-#                 return False
-            
-#             cur_word += cur_letter
-            
-#             if cur_word != word[:len(cur_word)]:
-#                 return False
-            
-#             if cur_word == word:
-#                 return True
-            
-#             if len(cur_word) >= len(word):
-#                 return False
-            
-#             board[x][y] = '*'
-#             neigh = [(x + k[0], y + k[1]) for k in directions]
-            
-#             for nb in neigh:
-#                 if 0 <= nb[0] and nb[0] < m and 0 <= nb[1] and nb[1] < n:
-#                     next_l = board[nb[0]][nb[1]]
-#                     if len(cur_word) <= len(word):
-#                         if search(nb[0], nb[1], cur_word):
-#                             board[x][y] = cur_letter
-#                             return True
-                        
-#             board[x][y] = cur_letter
-#             return False
-        
-#         directions = [(0,1), (0,-1), (1,0), (-1,0)]
-        
-#         m = len(board)
-#         n = len(board[0])
-        
-#         for i in range(m):
-#             for j in range(n):
-#                 if search(i,j,''):
-#                     return True
-        
-#         return False
-        
+        return self.ans
