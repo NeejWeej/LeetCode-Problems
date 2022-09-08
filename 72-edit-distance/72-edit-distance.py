88817f7b42450ae06e2f73ea3d32b2828234e32a
NeejWeej <nijat.khanbabayev@gmail.com>
2022-09-07 23:13:44 -0400

    Time: 195 ms (68.99%), Space: 23.6 MB (8.15%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -2,21 +2,21 @@ class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         n = len(word1)
         m = len(word2)
-        if n == 0 or m == 0:
-            return max(n, m)
+        if n < m:
+            n, m, word1, word2 = m, n, word2, word1
+            
+        if m == 0:
+            return n
         
         dp = [[float('inf') for _ in range(m + 1)] for _ in range(n + 1)]
         
         dp[0][0] = 0
-        for i in range(min(n,m) + 1):
+        for i in range(m + 1):
             dp[i][0] = i
             dp[0][i] = i
         
-        for i in range(min(n,m) + 1, max(n, m) + 1):
-            if i <= n:
-                dp[i][0] = i
-            if i <= m:
-                dp[0][i] = i
+        for i in range(m + 1, n + 1):
+            dp[i][0] = i
         
         for i in range(n):
             for j in range(m):
