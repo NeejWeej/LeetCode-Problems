e5ff08cdac8e2329b4092e3cfa3b3112762bdde6
NeejWeej <nijat.khanbabayev@gmail.com>
2022-09-07 22:52:48 -0400

    Time: 2690 ms (43.62%), Space: 15.6 MB (74.19%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.py
+++ 212-word-search-ii/212-word-search-ii.py
@@ -1,74 +1,52 @@
-class Solution: 
-    def makeTrie(self, words):
-        self.root = {}
-        for word in words:
-            cur = self.root
-            for c in word:
-                if c in cur:
-                    nextt = cur.get(c)
-                else:
-                    nextt = {}
-                    cur[c] = nextt
-                    nextt['^'] = cur
-                cur = nextt
-            cur['+'] = True
-        return
-    
-    def remove(self, word, node):
-        cur = node
-        del cur['+']
-        letter_idx = len(word) - 1
-        while len(cur) == 1:
-            p = cur.get('^')
-            if not p:
-                return
-            del p[word[letter_idx]]
-            letter_idx -= 1
-            del cur
-            cur = p
-        return
-                
-                
-    def search(self, r, c, cur, path, neighs, board):
-        # self.visited.add((r,c))
-        temp = board[r][c]
-        board[r][c] = '$'
-        if '+' in cur:
-            new_word = "".join(path)
-            self.ans.append(new_word)
-            self.remove(new_word, cur)
-        if len(self.root) == 0:
-            return
-        for dx, dy in neighs:
-            if not (0 <= r + dx < len(board) and 0<= c + dy < len(board[0])):
-                continue
-            # if (r + dx, c + dy) in self.visited:
-            #     continue
-            next_letter = board[r + dx][c + dy]
-            if next_letter not in cur:
-                continue
-            new_cur = cur.get(next_letter)
-            path.append(next_letter)
-            self.search(r + dx, c + dy, new_cur, path, neighs, board)
-        board[r][c] = temp
-        path.pop()
-        return
-             
+class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        self.makeTrie(words)
-        self.ans = []
-        neighs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        trie = {}
         m = len(board)
         n = len(board[0])
-        # self.visited = set()
-        for r in range(m):
-            for c in range(n):
-                letter = board[r][c]
-                if letter not in self.root:
-                    continue
-                node = self.root.get(letter)
-                self.search(r, c, node, [letter], neighs, board)
-                if len(self.root) == 0:
-                    return self.ans
-        return self.ans
+        for word in words:
+            cur = trie
+            for c in word:
+                cur[c] = cur.get(c, {})
+                old = cur
+                cur = cur.get(c)
+                cur['parent'] = old
+                cur['val'] = c
+            cur['#'] = word
                 
+        direc = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        self.ans = []
+        # print(trie)
+        def delete(node):
+            while len(node) == 2 and node is not trie:
+                char = node.get('val')
+                node = node.get('parent')
+                del node[char]
+            return node
+                
+        
+        def search(curChar, r, c):
+            old = board[r][c]
+            board[r][c] = '$'
+            node = curChar
+            if '#' in curChar:
+                self.ans.append(curChar.get('#'))
+                del curChar['#']
+                node = delete(curChar)
+            
+            if node == curChar:
+                for dx, dy in direc:
+                    if 0<= r+dx < m and 0<=c+dy<n:
+                        val = board[r + dx][c + dy]
+                        if val in curChar:
+                            search(curChar.get(val), r+dx, c+dy)
+            board[r][c] = old
+        
+        for i in range(m):
+            for j in range(n):
+                char = board[i][j]
+                if char in trie:
+                    search(trie.get(char), i, j)
+        
+        return self.ans
+        
+        
