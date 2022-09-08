e8a0d90c81bcbe348839e559e4d61ea67f876391
NeejWeej <nijat.khanbabayev@gmail.com>
2022-09-07 23:42:47 -0400

    Time: 78 ms (39.83%), Space: 14.2 MB (68.20%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -3,17 +3,17 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
         best = 0
-        dp = [0 for _ in range(n)]
-        for i in range(n-2, -1, -1):
-            bestBuy = prices[i]
-            bestVal = 0
-            for j in range(i + 1, n):
-                if prices[j] < bestBuy:
-                    bestBuy = prices[j]
-                else:
-                    nextStart = dp[j + 2] if j + 2 < n else 0
-                    bestVal = max(bestVal, prices[j] - bestBuy + nextStart)
-            dp[i] = bestVal
-        return dp[0]
+        bestBuy = lastBuy = -prices[0]
+        lastSell = float('-inf')
+        lastCoolDown = 0
+        
+        for p in prices:
+            cd = max(bestBuy, lastSell, lastCoolDown)
+            buy = lastCoolDown - p
+            sell = bestBuy + p
+            bestBuy = max(buy, bestBuy)
+            lastBuy, lastCoolDown, lastSell = buy, cd, sell
+        
+        return max(lastSell, lastCoolDown)
                 
             
