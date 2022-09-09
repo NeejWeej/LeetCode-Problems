f0fd7e18636a3cd1ad25a06c5e12d6e3ded9e1eb
NeejWeej <nijat.khanbabayev@gmail.com>
2022-09-09 01:24:05 -0400

    Time: 1035 ms (91.88%), Space: 30.1 MB (40.72%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
@@ -1,30 +1,46 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        stack = []
+        stack = [(0, -1)]
         n = len(heights)
-        rightExtend = [0] * n
+        best = 0
+        for i,h in enumerate(heights):
+            while h < stack[-1][0]:
+                oldH, idx = stack.pop()
+                idxBefore = stack[-1][1]
+                best = max(best, oldH *(i - idxBefore - 1))
+            stack.append((h, i))
         
-        for i,v in enumerate(heights):
-            while stack and v < stack[-1][1]:
-                idx, lastVal = stack.pop()
-                rightExtend[idx] = i - idx
-            stack.append((i, v))
+        for i in range(1, len(stack)):
+            h = stack[i][0]
+            last = stack[i-1][1]
+            best = max(best, h * (n - 1 - last))
         
-        while stack:
-            idx, lastVal = stack.pop()
-            rightExtend[idx] = n - idx 
+        return best
+                
+                
+#         rightExtend = [0] * n
         
-        ans = 0
-        for i,v in enumerate(reversed(heights)):
-            while stack and v < stack[-1][1]:
-                idx, lastVal = stack.pop()
-                ans = max(ans, lastVal *(i - idx - 1 + rightExtend[-idx-1]))
-            stack.append((i,v))
-        while stack:
-            idx, lastVal = stack.pop()
-            ans = max(ans, lastVal * (n - idx - 1 + rightExtend[-idx-1]))
+#         for i,v in enumerate(heights):
+#             while stack and v < stack[-1][1]:
+#                 idx, lastVal = stack.pop()
+#                 rightExtend[idx] = i - idx
+#             stack.append((i, v))
+        
+#         while stack:
+#             idx, lastVal = stack.pop()
+#             rightExtend[idx] = n - idx 
+        
+#         ans = 0
+#         for i,v in enumerate(reversed(heights)):
+#             while stack and v < stack[-1][1]:
+#                 idx, lastVal = stack.pop()
+#                 ans = max(ans, lastVal *(i - idx - 1 + rightExtend[-idx-1]))
+#             stack.append((i,v))
+#         while stack:
+#             idx, lastVal = stack.pop()
+#             ans = max(ans, lastVal * (n - idx - 1 + rightExtend[-idx-1]))
             
-        return ans
+#         return ans
             
         
             
