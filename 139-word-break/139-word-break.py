83af129b7e6b208a33dd4619300bdc5f29f7152b
NeejWeej <nijat.khanbabayev@gmail.com>
2022-09-09 01:46:34 -0400

    Time: 74 ms (27.56%), Space: 13.9 MB (70.63%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,25 +1,18 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        words = set(wordDict)
-        max_word, min_word = -1, float('inf')
-        for word in words:
-            max_word = max(len(word), max_word)
-            min_word = min(len(word), min_word)
-        if s in words:
-            return True
-        dp = [False for _ in range(len(s) + 1)]
-        for end in range(1, len(s) + 1):
-            first_idx = max(1, end - max_word)
-            last_idx = min(end, end - min_word + 1)
-            if s[:end] in words:
-                dp[end] = True
-            else:
-                for start in range(first_idx, last_idx):
-                    ending_word = s[start: end]
-                    if ending_word not in words:
-                        continue
-                    if dp[start]:
-                        dp[end] = True
-                        break
-            dp[end] = dp[end] or False
+        n = len(s)
+        dp = [False for _ in range(n)]
+        mod = 10**9 + 7
+        prime = 31
+        
+        d = set(wordDict)
+        
+        checkPoints = [-1]
+        for i,c in enumerate(s):
+            for c in checkPoints:
+                start = c + 1
+                if s[start: i + 1] in d:
+                    dp[i] = True
+                    checkPoints.append(i)
+                    break
         return dp[-1]
