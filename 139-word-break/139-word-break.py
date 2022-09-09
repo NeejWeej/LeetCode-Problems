5cedf7b71741f6d6c03809851a0f8b9c4114b4e5
NeejWeej <nijat.khanbabayev@gmail.com>
2022-09-09 01:49:05 -0400

    Time: 64 ms (46.00%), Space: 14 MB (70.63%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -4,12 +4,14 @@ class Solution:
         dp = [False for _ in range(n)]
         mod = 10**9 + 7
         prime = 31
-        
         d = set(wordDict)
         
         checkPoints = [-1]
-        for i,c in enumerate(s):
-            for c in checkPoints:
+        for i,char in enumerate(s):
+            for j in range(len(checkPoints) - 1, -1, -1):
+                c = checkPoints[j]
+                if i - c > 20:
+                    break
                 start = c + 1
                 if s[start: i + 1] in d:
                     dp[i] = True
