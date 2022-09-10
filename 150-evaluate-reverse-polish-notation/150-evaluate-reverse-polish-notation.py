b951e4788afff7feabdce9213b550b96e01663d1
NeejWeej <nijat.khanbabayev@gmail.com>
2022-09-10 18:10:06 -0400

    Time: 61 ms (99.29%), Space: 14.4 MB (63.49%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
@@ -1,23 +1,31 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
-        for token in tokens:
-            if (len(token) > 1 and token[0] == '-') or token.isnumeric():
-                stack.append(int(token))
-                continue
-            num1 = stack.pop()
-            num2 = stack.pop()
-            if token == '+':
-                stack.append(num1 + num2)
-            elif token == '-':
-                stack.append(num2 - num1)
-            elif token == '*':
-                stack.append(num1 * num2)
-            elif token == '/':
-                ans = abs(num2) // abs(num1)
-                if num2 < 0:
-                    ans *= -1
-                if num1 < 0:
-                    ans *= -1
-                stack.append(ans)
-        return stack[0]
+        ops = set(['+', '-', '*', '/'])
+        
+        for tok in tokens:
+            if tok in ops:
+                r = stack.pop()
+                l = stack.pop()
+                if tok == '+':
+                    stack.append(l + r)
+                    
+                elif tok == '-':
+                    stack.append(l - r)
+                    
+                elif tok == '*':
+                    stack.append(l*r)
+                    
+                elif tok == "/":
+                    if l * r >= 0:
+                        stack.append(l // r)
+                        
+                    else:
+                        if l%r == 0:
+                            stack.append(l // r)
+                        else:
+                            stack.append(1 + (l // r))
+            else:
+                stack.append(int(tok))
+                
+        return stack[-1]
