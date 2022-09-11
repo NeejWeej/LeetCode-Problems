ed17fdabda1583592639d7760f24daf88865c828
NeejWeej <nijat.khanbabayev@gmail.com>
2022-09-11 03:47:10 -0400

    Time: 617 ms (27.57%), Space: 17.6 MB (9.34%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -13,40 +13,34 @@ class Solution:
         
         self.ans = []
         def dfs(r, c, height, visited, oldVisited):
+            if (r,c) in visited:
+                return
+            visited.add((r,c))
             if oldVisited and (r,c) in oldVisited:
                 self.ans.append([r,c])
                 
             for dx, dy in direc:
                 if valid(r+dx, c+dy) and heights[r+dx][c+dy]>=height:
                     if (r+dx,c+dy) not in visited:
-                        visited.add((r+dx,c+dy))
                         dfs(r+dx, c+dy, heights[r+dx][c+dy], visited, oldVisited)
         
         pacificV = set()
         for c in range(n):
             h = heights[0][c]
-            if (0, c) not in pacificV:
-                pacificV.add((0,c))
-                dfs(0, c, h, pacificV, None)
+            dfs(0, c, h, pacificV, None)
             
         for r in range(m):
             h = heights[r][0]
-            if (r, 0) not in pacificV:
-                pacificV.add((r,0))
-                dfs(r, 0, h, pacificV, None)
+            dfs(r, 0, h, pacificV, None)
         
         atl = set()
         for c in range(n):
             h = heights[-1][c]
-            if (m-1,c) not in atl:
-                atl.add((m-1, c))
-                dfs(m-1, c, h, atl, pacificV)
+            dfs(m-1, c, h, atl, pacificV)
             
         for r in range(m):
             h = heights[r][-1]
-            if (r, n-1) not in atl:
-                atl.add((r, n-1))
-                dfs(r, n-1, h, atl, pacificV)        
+            dfs(r, n-1, h, atl, pacificV)        
         
         
         return self.ans
