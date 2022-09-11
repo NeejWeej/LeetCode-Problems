a5f3ffbfaab6255fc51235adacc95b9cb20f6d30
NeejWeej <nijat.khanbabayev@gmail.com>
2022-09-11 03:44:32 -0400

    Time: 726 ms (18.91%), Space: 17.7 MB (8.13%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -1,61 +1,60 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        stack = []
         m = len(heights)
         n = len(heights[0])
-        if n == 1 or m == 1:
-            return [[x,y] for x in range(m) for y in range(n)]
-        counts = {}
-        delta_n = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        def valid_neighs(x,y):
-            neighs = []
-            for dx, dy in delta_n:
-                if 0 <= dx + x < m and 0 <= dy + y < n:
-                    neighs.append((dx + x, dy + y))
-            return neighs
         
-        def dfs(shift, already_seen_region):
-            while stack:
-                x, y, val = stack.pop()[:]
-                neighs = valid_neighs(x,y)
-                for nx, ny in neighs:
-                    nval = heights[nx][ny]
-                    if counts.get((nx, ny), 0) not in already_seen_region:
-                        if nval >= val:
-                            stack.append((nx, ny, nval))
-                            counts[(nx,ny)] = counts.get((nx, ny), 0) + shift
-                            
-        def set_row_col(row, v, shift):
-            if row:
-                for i in range(n):
-                    if i == 0 and v == 0:
-                        continue
-                    if i == n - 1 and v == m - 1:
-                        continue
-                    stack.append((v,i,heights[v][i]))
-                    counts[(v,i)] = counts.get((v,i), 0) + shift
-                    
-            else:
-                for i in range(m):
-                    stack.append((i,v, heights[i][v]))
-                    counts[(i,v)] = counts.get((i,v), 0) + shift
-                    
-        set_row_col(True, 0, 1)
-        set_row_col(False, 0, 1)
-        dfs(1, set([1]))
-
-        set_row_col(True, m - 1, 3)
-        set_row_col(False, n - 1, 3)
-        dfs(3, set([3, 4]))
-
-        ans = [[x,y] for (x,y), val in counts.items() if val == 4]
-        return ans
+        direc = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        
+        visited = set()
+        
+        def valid(r, c):
+            return 0<=r<m and 0<=c<n
+        
+        
+        self.ans = []
+        def dfs(r, c, height, visited, oldVisited):
+            if oldVisited and (r,c) in oldVisited:
+                self.ans.append([r,c])
+                
+            for dx, dy in direc:
+                if valid(r+dx, c+dy) and heights[r+dx][c+dy]>=height:
+                    if (r+dx,c+dy) not in visited:
+                        visited.add((r+dx,c+dy))
+                        dfs(r+dx, c+dy, heights[r+dx][c+dy], visited, oldVisited)
+        
+        pacificV = set()
+        for c in range(n):
+            h = heights[0][c]
+            if (0, c) not in pacificV:
+                pacificV.add((0,c))
+                dfs(0, c, h, pacificV, None)
+            
+        for r in range(m):
+            h = heights[r][0]
+            if (r, 0) not in pacificV:
+                pacificV.add((r,0))
+                dfs(r, 0, h, pacificV, None)
+        
+        atl = set()
+        for c in range(n):
+            h = heights[-1][c]
+            if (m-1,c) not in atl:
+                atl.add((m-1, c))
+                dfs(m-1, c, h, atl, pacificV)
+            
+        for r in range(m):
+            h = heights[r][-1]
+            if (r, n-1) not in atl:
+                atl.add((r, n-1))
+                dfs(r, n-1, h, atl, pacificV)        
+        
+        
+        return self.ans
         
         
             
             
             
             
-        
-        
+            
         
