b1016d5a702fa86bab9904b11e7deaa0c0cd3ac9
NeejWeej <nijat.khanbabayev@gmail.com>
2022-09-12 16:25:59 -0400

    Time: 53 ms (43.74%), Space: 13.8 MB (67.62%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.py
@@ -1,20 +1,20 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        lb = ub = 0
+        uB = lB = 0
         
         for char in s:
             if char == '(':
-                ub += 1
-                lb += 1
+                uB += 1
+                lB += 1       
                 
             elif char == ')':
-                ub -= 1
-                lb = max(0, lb - 1)
-                if ub < 0:
+                uB -= 1
+                lB = max(0, lB - 1)
+                if uB < 0:
                     return False
-            
+                
             elif char == '*':
-                lb = max(0, lb - 1)
-                ub += 1
-        
-        return lb <= 0 <= ub
+                uB += 1
+                lB = max(0, lB - 1)
+                
+        return lB <= 0 <= uB
