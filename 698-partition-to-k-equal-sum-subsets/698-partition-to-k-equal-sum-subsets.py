11146966df8c2a18241524e6fd1d986dffccaf0c
NeejWeej <nijat.khanbabayev@gmail.com>
2022-09-12 02:26:24 -0400

    Time: 171 ms (73.77%), Space: 13.8 MB (95.49%) - LeetHub

---
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        target, m = divmod(sum(nums), k)
+        if m: return False
+        dp, n = [0]*k, len(nums)
+        nums.sort(reverse=True)
+        def dfs(i):
+            if i == n:
+                return len(set(dp)) == 1
+            for j in range(k):
+                dp[j] += nums[i]
+                if dp[j] <= target and dfs(i+1):
+                    return True
+                dp[j] -= nums[i]
+                if not dp[j]: break
+            return False
+        return nums[0] <= target and dfs(0)
