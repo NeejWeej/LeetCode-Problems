6e282db88677b94f6abe19f87dd0e7928a46e69e
NeejWeej <nijat.khanbabayev@gmail.com>
2022-09-13 03:08:59 -0400

    Time: 558 ms (42.59%), Space: 14.3 MB (73.78%) - LeetHub

--- 494-target-sum/494-target-sum.py
+++ 494-target-sum/494-target-sum.py
@@ -3,23 +3,40 @@ class Solution:
         self.ans = 0
         n = len(nums)
         dp = {}
+        dp = collections.defaultdict(int)
         
-        def dfs(nums, idx, target):
-            if (idx, target) in dp:
-                return dp.get((idx, target))
+        it = iter(nums)
+        first = next(it)
+        if first == 0:
+            dp[first] = 2
+        else:
+            dp[first] = 1
+            dp[-first] = 1
+        for num in it:
+            # print(dp)
+            newDP = collections.defaultdict(int)
+            for tot, val in dp.items():
+                newDP[tot + num] += val
+                newDP[tot - num] += val
+            dp = newDP
+        return dp.get(target, 0)
+        
+#         def dfs(nums, idx, target):
+#             if (idx, target) in dp:
+#                 return dp.get((idx, target))
             
-            if idx == n:
-                if target == 0:
-                    dp[(idx, target)] = 1
-                    return 1
-                else:
-                    dp[(idx, target)] = 0
-                    return 0
+#             if idx == n:
+#                 if target == 0:
+#                     dp[(idx, target)] = 1
+#                     return 1
+#                 else:
+#                     dp[(idx, target)] = 0
+#                     return 0
                 
-            else:
-                left = dfs(nums, idx + 1, target - nums[idx])
-                right = dfs(nums, idx + 1, target + nums[idx])
-                dp[(idx, target)] = left + right
-                return left + right
+#             else:
+#                 left = dfs(nums, idx + 1, target - nums[idx])
+#                 right = dfs(nums, idx + 1, target + nums[idx])
+#                 dp[(idx, target)] = left + right
+#                 return left + right
         
-        return dfs(nums, 0, target)
+#         return dfs(nums, 0, target)
