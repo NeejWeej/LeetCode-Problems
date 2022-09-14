c69fc68992a3d5058bd0aeaf77104ae34dd7cd55
NeejWeej <nijat.khanbabayev@gmail.com>
2022-09-14 00:47:44 -0400

    Time: 1874 ms (24.82%), Space: 21.2 MB (81.27%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -2,9 +2,6 @@ class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         # return sum(i - j - fee for i,j in zip(prices[1:], prices[:-1]) if i -j - fee > 0)
         n = len(prices)
-        if n < 2:
-            return 0
-        dp = [0]*n
         # we can combine transactions b1,s1 b2,s2 if s1 - b1 - fee + s2 - b2 - fee < s2 - b1 - fee
         # s1 - b2 - fee < 0
         
