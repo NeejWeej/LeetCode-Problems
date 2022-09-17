d5a6e302a16b7075d74c6868465faeaac00fac5c
NeejWeej <nijat.khanbabayev@gmail.com>
2022-09-17 02:34:51 -0400

    Time: 6099 ms (17.95%), Space: 147.8 MB (32.50%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -0,0 +1,36 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        cost = 0
+        n = len(points)
+        if n <= 1:
+            return 0
+        
+        included = set()
+        
+        distances = []
+        
+        nodeToDist = collections.defaultdict(list)
+        
+        for i, p in enumerate(points):
+            tupP = tuple(p)
+            for j in range(i + 1, n):
+                otherP = tuple(points[j])
+                dist = abs(p[0] - otherP[0]) + abs(p[1] - otherP[1]) 
+                nodeToDist[tupP].append((dist, otherP))
+                nodeToDist[otherP].append((dist, tupP))
+        
+        
+        distances = nodeToDist[tuple(points[0])]
+        heapq.heapify(distances)
+        included.add(tuple(points[0]))
+        
+        while len(included) < n:
+            d, p1 = heapq.heappop(distances)
+            if p1 not in included:
+                cost += d
+                included.add(p1)
+                for pair in nodeToDist.get(p1):
+                    heapq.heappush(distances, pair)
+                    
+        return cost
+                
