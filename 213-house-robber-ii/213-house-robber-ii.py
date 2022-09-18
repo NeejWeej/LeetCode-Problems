eadde0e2992f8bf23029f03b81329dfcc3a21872
NeejWeej <nijat.khanbabayev@gmail.com>
2022-09-18 16:42:38 -0400

    Time: 52 ms (50.85%), Space: 13.8 MB (70.83%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -4,17 +4,20 @@ class Solution:
         n = len(nums)
         if n <= 2:
             return max(nums)
-        dp = [0]*n
-        dp[0] = nums[0]
-        last2 = 0
-        for idx, num in enumerate(islice(nums, 1, n - 1), 1):
-            dp[idx] = max(num + last2, dp[idx - 1])
-            last2 = dp[idx - 1]
-        bestWith = dp[-2]
-        dp[0] = nums[1]
-        last2 = 0
-        for idx, num in enumerate(islice(nums, 2, n), 2):
-            dp[idx - 1] = max(num + last2, dp[idx - 2])
-            last2 = dp[idx - 2]
-        return max(bestWith, dp[-2])
+        
+        last = nums[1]
+        last2 = nums[0]
+        
+        for val in itertools.islice(nums, 2, n - 1):
+            last, last2 = max(val + last2, last), max(last, last2)
+        
+        best = max(last2, last)
+        last = nums[2]
+        last2 = nums[1]
+        
+        for val in itertools.islice(nums, 3, n):
+            last, last2 = max(val + last2, last), max(last, last2)
+        
+        return max(best, last, last2)
+            
             
