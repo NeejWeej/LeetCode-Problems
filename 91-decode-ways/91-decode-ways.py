5ed6fa52c83eded300294b908c1ee6d8d9f709c2
NeejWeej <nijat.khanbabayev@gmail.com>
2022-09-18 16:27:14 -0400

    Time: 65 ms (21.85%), Space: 13.9 MB (46.16%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,26 +1,31 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
+        n = len(s)
         if s[0] == '0':
             return 0
-        if len(s) == 1:
+        
+        if n == 1:
             return 1
-        last = 1
-        last2 = 1
-        can_have = set(['1','2'])
-        can_have_2 = set(['0','1', '2','3', '4', '5','6'])
-        for idx in range(1, len(s)):
-            if s[idx] == '0':
-                if s[idx - 1] not in can_have:
-                    return 0
-                else:
-                    last, last2 = last2, last
-            else:
-                cur = last
-                if s[idx - 1] == '1':
-                    cur += last2
-                elif s[idx - 1] == '2' and s[idx] in can_have_2:
-                    cur += last2
-                last, last2 = cur, last
-        return last
-    
+        
+        dp = [0]*n
+        if s[-1] != '0':
+            dp[-1] = 1
+        if s[-2] != '0':
+            dp[-2] = dp[-1]
+            if int(s[-2:]) <= 26:
+                dp[-2] += 1
+        
+        enum =  enumerate(reversed(s))
+        # do this to not copy a reversed form of the string
+        next(enum)
+        next(enum)
+        
+        for revI, val in enum:
+            i = n - revI - 1
+            if val != '0':
+                dp[i] = dp[i + 1]
+                if int(val + s[i + 1]) <= 26:
+                    dp[i] += dp[i + 2]
+                    
+        return dp[0]
         
