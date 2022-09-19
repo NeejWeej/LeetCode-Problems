1b5a8f75442c9a31afd1f2cbfa9228c34d929878
NeejWeej <nijat.khanbabayev@gmail.com>
2022-09-19 13:49:34 -0400

    Time: 2195 ms (58.27%), Space: 28.9 MB (52.20%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,25 +1,13 @@
 class Solution:
-    from itertools import islice
     def maxProfit(self, prices: List[int]) -> int:
-        n = len(prices)
+        dp_1_hold, dp_1_not_hold = float('-inf'), 0
+        dp_2_hold, dp_2_not_hold = float('-inf'), 0
         
-        forwardDp = [0 for _ in range(n)]
-        min_so_far = prices[0]
-        for i,p in enumerate(islice(prices, 1, n), 1):
-            forwardDp[i] = max(forwardDp[i - 1], p - min_so_far)
-            min_so_far = min(p, min_so_far)
-        
-        backDp = [0 for _ in range(n)]
-        best_sell = prices[-1]
-        enum = enumerate(reversed(prices), 1)
-        # skip first entry in enumerate WITHOUT copying to a new list
-        next(enum)
-        for i, p in enum:
-            idx = n - i
-            backDp[idx] = max(backDp[idx + 1], best_sell - p)
-            best_sell = max(p, best_sell)
+        for stock_price in prices:    
+            dp_1_hold = max(dp_1_hold, -stock_price)
+            dp_1_not_hold = max(dp_1_not_hold, dp_1_hold + stock_price)
             
-        profit = 0
-        for i in range(n):
-            profit = max(profit, forwardDp[i] + backDp[i])
-        return profit
+            dp_2_hold = max(dp_2_hold, dp_1_not_hold - stock_price)
+            dp_2_not_hold = max(dp_2_not_hold, dp_2_hold + stock_price)
+        
+        return dp_2_not_hold
