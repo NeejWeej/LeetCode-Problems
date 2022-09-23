526ff657361b69edf00c914e37bb5613e75a5c47
NeejWeej <nijat.khanbabayev@gmail.com>
2022-09-23 12:52:56 -0400

    Time: 529 ms (20.48%), Space: 78.6 MB (63.91%) - LeetHub

--- 853-car-fleet/853-car-fleet.cpp
+++ 853-car-fleet/853-car-fleet.cpp
@@ -20,20 +20,18 @@ public:
         
         sort(sp.begin(), sp.end(), Solution::comparePos);
         
-        vector<double> stack;
-        
-        stack.push_back(destTime(target, sp[0]));
+        double latestTime = destTime(target, sp[0]);
+        int count = 1;
         
         for(auto& car: sp){
             double arrival = destTime(target, car);
             // cout<< arrival << " " << stack.back() << "\n";
-            if (arrival > stack.back()) stack.push_back(arrival);
+            if (arrival > latestTime){
+                latestTime = arrival;
+                count++;
+            }
         }
-        // cout<<"\n";
-        // for(auto& p: sp) cout<< get<0>(p) << ","<< get<1>(p) << " ";
-        // cout << "\n";
-        // for(auto t: stack) cout<< t<< " ";
         
-        return stack.size();
+        return count;
     }
 };
