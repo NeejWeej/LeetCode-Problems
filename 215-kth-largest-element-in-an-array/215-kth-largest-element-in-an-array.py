debbeb7e6daf0bbf12dd5559529a41a7b585f016
NeejWeej <nijat.khanbabayev@gmail.com>
2022-09-23 14:20:24 -0400

    Time: 891 ms (55.52%), Space: 28 MB (32.30%) - LeetHub

---
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -0,0 +1,34 @@
+import random
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        
+        def quick(count, vals):
+            left = []
+            right = []
+            equal = []
+            
+            key = random.choice(vals)
+            
+            for val in vals:
+                if val < key:
+                    left.append(val)
+                elif val == key:
+                    equal.append(val)
+                else:
+                    right.append(val)
+            
+            if len(right) < count:
+                if len(right) + len(equal) >= count:
+                    return key
+                else:
+                    return quick(count - len(right) - len(equal), left)
+            
+            else:
+                return quick(count, right)
+        
+        return quick(k, nums)
+            
+                
+            
+                
