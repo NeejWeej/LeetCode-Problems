cbc79b96b64c838015e3f652dddef68f7ed59072
NeejWeej <nijat.khanbabayev@gmail.com>
2022-10-24 14:46:11 -0400

    Time: 405 ms (25.11%), Space: 103.6 MB (6.10%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,45 +1,28 @@
+struct tempIdx {
+    int temp;
+    int idx;
+    
+    tempIdx(int t, int i): temp(t), idx(i) {}
+};
+
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& T) {
-        if (T.empty())
-        {
-            return vector<int>();
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> res(n, 0);
+        
+        vector<tempIdx> data;
+        int counter = 0;
+        for(int idx = 0; idx<n; ++idx){
+            int temp = temperatures[idx];
+            while(data.size() > 0){
+                tempIdx& end = data.back();
+                if (end.temp >= temp) break;
+                res[end.idx] = (idx - end.idx);
+                data.pop_back();
+            }
+            data.emplace_back(temp, idx);
         }
-        
-		// A stack with values:  
-		// <T[i], Number of days to the next larger number in T>
-        stack<pair<int, int>> s;
-		
-		// The last number in T must have no value larger than it.
-        s.push(make_pair(T.back(), 0));
-        
-        vector<int> ans(T.size(), 0);
-        
-		// Start from the back.
-        for (int i = T.size() - 2; i >= 0; i--)
-        {
-            int counter = 1;
-            
-			// Pop the stack until its empty or the top number is smaller or equal to T[i]
-			// The counter + the number of elements to the next larger number in T.
-            while (!s.empty() && T[i] >= s.top().first)
-            {
-                counter += s.top().second;
-                s.pop();
-            }
-            
-			// If no number on stack is larger than T[i].
-            if (s.empty())
-            {
-                s.push(make_pair(T[i], 0));
-            }
-            else
-            {
-                s.push(make_pair(T[i], counter));
-                ans[i] = counter;
-            }
-        }
-        
-        return ans;
+        return res;
     }
 };
