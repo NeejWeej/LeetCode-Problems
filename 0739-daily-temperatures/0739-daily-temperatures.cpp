7ba4e9ddc38d1601d1533a8071659dea983e7d1f
NeejWeej <nijat.khanbabayev@gmail.com>
2022-10-24 14:44:16 -0400

    Time: 461 ms (8.08%), Space: 98.7 MB (9.54%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,31 +1,30 @@
 class Solution {
-    
-struct tempIdx {
-    int temp;
-    int idx;
-    
-    tempIdx(int t, int i): temp(t), idx(i) {}
-};
-    
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        int n = temperatures.size();
-        vector<int> res(n, 0);
         
-        vector<tempIdx> data;
-        int counter = 0;
-        for(int idx = 0; idx<n; ++idx){
-            int temp = temperatures[idx];
-            while(data.size() > 0){
-                tempIdx& end = data.back();
-                if (end.temp >= temp){
-                    break;
-                }
-                res[end.idx] = (idx - end.idx);
-                data.pop_back();
-            }
-            data.emplace_back(temp, idx);
+        int n = temperatures.size();
+        vector<int>nge(n, 0); // initially all 0, stores distance between their next greater element and current temperature
+        stack<int>st{};
+        
+		// move from right to left
+        for(int i = n-1; i>=0; --i){
+            // pop until we find next greater element to the right
+			// since we came from right stack will have element from right only
+			// s.top() is the index of elements so we put that index inside temperatures vector to check
+            while(!st.empty() && temperatures[st.top()] <= temperatures[i])
+                st.pop();
+				
+            // if stack not empty, then we have some next greater element, 
+			// so we take distance between next greater and current temperature
+			// as we are storing indexes in the stack
+            if(!st.empty())
+                nge[i] = st.top()-i; // distance between next greater and current
+            
+			// push the index of current temperature in the stack,
+			// same as pushing current temperature in stack
+            st.push(i);
         }
-        return res;
+        
+        return nge;
     }
 };
